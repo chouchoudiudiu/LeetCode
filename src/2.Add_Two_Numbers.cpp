2bfc6c29ca7a64759b6a982ec44f95311b4b2476
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-03-13 19:52:42 -0700

    Update 2.Add_Two_Numbers.cpp

--- src/2.Add_Two_Numbers.cpp
+++ src/2.Add_Two_Numbers.cpp
@@ -14,13 +14,11 @@ public:
         ListNode* p = dummy;
         while (l1 || l2 || carry > 0)
         {
-            sum = (l1?l1->val : 0) + (l2?l2->val : 0) + carry;
-            
+            sum = (l1?l1->val : 0) + (l2?l2->val : 0) + carry;    
             ListNode* node = new ListNode(sum%10);
             carry = sum/10;
             l1 = l1 ? l1->next : NULL;
-            l2 = l2 ? l2->next : NULL;
-           
+            l2 = l2 ? l2->next : NULL;       
             p->next = node;
             p = node;
         }
