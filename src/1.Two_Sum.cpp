e859ca28d2afab283e455d1a95583136d5f329f6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-03-13 17:03:14 -0700

    Update 1.Two_Sum.cpp

--- src/1.Two_Sum.cpp
+++ src/1.Two_Sum.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m; //num value, index
-        for(int i = 0; i < nums.size(); ++i)
-        {
+        for (int i = 0; i < nums.size(); ++i) {
             int val = target - nums[i];
-            if(m.count(val))
+            if (m.count(val))
                 return {m[val], i}; //1 solution only
             else
                 m.insert({nums[i], i});
