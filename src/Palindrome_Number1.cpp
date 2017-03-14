79822bcd8bce2b158cf33397a9d7921b678ea1d2
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-03-13 17:23:58 -0700

    Create Palindrome_Number1.cpp

---
+++ src/Palindrome_Number1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+	    if (n < 0 || (n != 0 && n%10 == 0)) //no leading zeros
+	        return false;
+	    int sum = 0;
+        while (n > sum) {
+            sum = sum*10 + n%10;
+            n /= 10;
+        }
+    
+        return sum == n || sum/10 == n; //even, odd; 坏处是 要比完所有？
+    }
+};
