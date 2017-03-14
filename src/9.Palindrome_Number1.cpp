e462abc3bdbd1252241099af7b4512421338b919
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-03-13 18:50:36 -0700

    Update 9.Palindrome_Number1.cpp

--- src/9.Palindrome_Number1.cpp
+++ src/9.Palindrome_Number1.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(int n) {
-	if (n < 0 || (n != 0 && n%10 == 0)) //no leading zeros
+    bool isPalindrome(int x) {
+	if (x < 0 || (x != 0 && x%10 == 0)) //no leading zeros
 	    return false;
 	int sum = 0;
-        while (n > sum) {
-            sum = sum*10 + n%10;
-            n /= 10;
+        while (x > sum) {
+            sum = sum*10 + x%10;
+            x /= 10;
         }
     
-        return sum == n || sum/10 == n; //even, odd; 坏处是 要比完所有？
+        return sum == x || sum/10 == x; //even, odd; 坏处是 要比完所有？
     }
 };
