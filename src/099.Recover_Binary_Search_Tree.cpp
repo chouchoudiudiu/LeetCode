9387e0ea7bd2504855dd1ad351a088b9543a9c3a
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-03-31 03:21:45 -0700

    Create 099.Recover_Binary_Search_Tree.cpp

---
+++ src/099.Recover_Binary_Search_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, TreeNode*& last, TreeNode*& node1, TreeNode*& node2)
+    {
+        if(!root)
+            return;
+        inorder(root->left, last, node1, node2);
+        if(!node1 && last->val > root->val) {
+            node1 = last;
+        }
+        if (last->val > root->val) {
+            node2 = root; //keep updating node2
+        }
+        last = root;
+        inorder(root->right, last, node1, node2);
+    }
+    void recoverTree(TreeNode* root) {
+        TreeNode* node1 = nullptr; //the first node smaller than previous node
+        TreeNode* node2 = nullptr; //the second node smaller than previous node
+        TreeNode* last = new TreeNode(INT_MIN); //always record the last node
+        inorder(root, last, node1, node2);
+        swap(node1->val, node2->val); // note not swap(node1, node2) or (*node1, *node2)... 
+    }
+};
