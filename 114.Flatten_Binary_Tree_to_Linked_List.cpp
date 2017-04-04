5dc247ce798792f58c2d2c0684d5d1ad2473e3b5
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-03 19:20:32 -0700

    Create 114.Flatten_Binary_Tree_to_Linked_List.cpp

---
+++ 114.Flatten_Binary_Tree_to_Linked_List.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+//一个树的根节点的Right先存起来，先求左边的flatten结果，记录最后访问的节点last，求好之后根的right为左子节点，last的右子节点为根的原//右子节点, 接着求右子树的flatten结果，因为root不变，自然的接到了last后面
+    void flatten(TreeNode* root) {
+        TreeNode* last = NULL;
+        helper(root, last);
+    }
+    void helper(TreeNode* root, TreeNode*& last) {
+        if (!root)
+            return;
+        last = root; //PREORDER
+        TreeNode* right = root->right;
+        helper(root->left, last);
+        root->right = root->left;
+        root->left = NULL;//CLOSE
+        last->right = right;
+        helper(root->right, last);
+    }
+};
