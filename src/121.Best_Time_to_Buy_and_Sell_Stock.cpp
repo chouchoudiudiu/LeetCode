b1efb27faaeae6ce8d94e0ffeef636fe5e9cf194
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-05 18:43:16 -0700

    Create 121.Best_Time_to_Buy_and_Sell_Stock.cpp

---
+++ src/121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+        int maxProfit = 0, minPrice = prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+};
