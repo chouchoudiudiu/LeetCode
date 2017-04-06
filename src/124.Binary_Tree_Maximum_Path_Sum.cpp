dc790132783d8b36652333cad97b64134177282b
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-05 19:35:41 -0700

    Create 124.Binary_Tree_Maximum_Path_Sum.cpp

---
+++ src/124.Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        if (!root)  
+            return 0;
+        int maxSum = INT_MIN;
+        helper(root, maxSum);
+        return maxSum;
+    }
+    int helper(TreeNode* root, int& maxSum) {
+        if (!root)
+            return 0;
+        int l = max(0, helper(root->left, maxSum));
+        int r = max(0, helper(root->right, maxSum));
+        maxSum = max(maxSum, l + r + root->val); //l + r + root->val is localmax
+        return root->val + max(l, r); 
+        //maxSum是包括左右两边以及根节点的最大值，l, r及ret的值，只是左半边+root,或者右半边+root, 为了后期拼接
+    }
+};
