ad4c182bfccab223584a1126b29b92875de2679a
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-07 19:29:59 -0700

    Create 153.Find_Minimum_in_Rotated_Sorted_Array.cpp

---
+++ src/153.Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if(nums.size() < 1)
+            return -1;
+        int low = 0, high = nums.size() - 1;
+        while (low <= high && nums[low] > nums[high]) { //okay to use low <= high or low < high
+            int mid = low + (high - low)/2;
+            if (nums[mid] < nums[high])
+                high = mid; //mid still possible
+            else
+                low = mid + 1;
+        }
+        return nums[low];
+    }
+};
