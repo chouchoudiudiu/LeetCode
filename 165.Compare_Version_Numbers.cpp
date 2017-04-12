27c1ba52b92e83ff8ed8dd8fc18bacc0050815b6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-12 09:46:26 -0700

    Create 165.Compare_Version_Numbers.cpp

---
+++ 165.Compare_Version_Numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        replace(version1.begin(), version1.end(), '.', ' ');
+        replace(version2.begin(), version2.end(), '.', ' ');
+        istringstream iss1(version1), iss2(version2); //or istringstream
+        while (true) {
+            int n1, n2;
+            bool e1 = (bool)(iss1 >> n1); //cast
+            bool e2 = (bool)(iss2 >> n2);
+            if (!e1 && !e2) //finished
+                return 0;
+            if (!e1)
+                n1 = 0; //n1 = 0! not return -1
+            if (!e2)
+                n2 = 0; //n2 = 0!
+            if (n1 > n2)
+                return 1;
+            if (n1 < n2)
+                return -1;
+        }
+        return 0;
+    }
+};
+/*
+"1.0"
+"1"
+*/
