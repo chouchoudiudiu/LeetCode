c52fdc53444a715892754b5eb1d86843ac0e773c
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-18 11:28:33 -0700

    Update 035.Search_Insert_Position.cpp

--- src/035.Search_Insert_Position.cpp
+++ src/035.Search_Insert_Position.cpp
@@ -4,18 +4,16 @@ public:
         int low = 0, high = nums.size() - 1;
         while (low <= high) {
             int mid = low + (high - low)/2;
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                low = mid + 1;
-            else
+            if (nums[mid] >= target)
                 high = mid - 1;
+            else
+                low = mid + 1;
         }
         return low;
     }
 };
 
 /*
-[1,3,5,5,5]
-7
+[1,2,5,5,5,5,5,5,5,5,5,8]
+5
 */
