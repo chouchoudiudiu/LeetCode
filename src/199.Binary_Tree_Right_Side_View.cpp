7f3b3f789287b6a360978160f1ea4b4ac298f4b4
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-04-20 19:38:02 -0700

    Create 199.Binary_Tree_Right_Side_View.cpp

---
+++ src/199.Binary_Tree_Right_Side_View.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& res, int depth) {
+        if (!root)
+            return;
+        if (depth == res.size())
+            res.push_back(root->val);
+        dfs(root->right, res, depth + 1);
+        dfs(root->left, res, depth + 1); //if showing left side view, put left and then right
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        dfs(root, res, 0);
+        return res;
+    }
+};
