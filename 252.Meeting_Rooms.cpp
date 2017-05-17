9007a1fe26ab060798e24ac19a8e2161a6d15a42
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-17 03:59:02 -0700

    Create 252.Meeting_Rooms.cpp

---
+++ 252.Meeting_Rooms.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+bool compare(Interval a, Interval b) {
+    return a.start < b.start;
+}
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), compare);
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start < intervals[i - 1].end)
+                return false;
+        }
+        return true;
+    }
+};
