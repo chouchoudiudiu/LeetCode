805619c0152de153eb67c1f56061d0190a915e39
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-16 23:40:20 -0700

    Create 244.Shortest_Word_Distance_II.cpp

---
+++ src/244.Shortest_Word_Distance_II.cpp
@@ -0,0 +1,28 @@
+class WordDistance {
+public:
+    WordDistance(vector<string> words) {
+        for (int i = 0; i < words.size(); ++i) {
+            m[words[i]].push_back(i);
+        }
+    }
+    
+    int shortest(string word1, string word2) {
+        int i = 0, j = 0, minD = INT_MAX;
+        while (i < m[word1].size() && j < m[word2].size()) {
+            minD = min(minD, abs(m[word1][i] - m[word2][j]));
+            if (m[word1][i] < m[word2][j])
+                ++i;
+            else
+                ++j;
+        }
+        return minD;
+    }
+    
+    unordered_map<string, vector<int>> m;
+};
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance obj = new WordDistance(words);
+ * int param_1 = obj.shortest(word1,word2);
+ */
