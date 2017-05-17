d2708f1d1c9e0ba48292900db4a87d3db874df51
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-16 22:34:41 -0700

    Create 229.Majority_Element_II.cpp

---
+++ src/229.Majority_Element_II.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+// Moore Voting
+    vector<int> majorityElement(vector<int>& nums) {
+        int c1 = 0, c2 = 0, m1 = 0, m2 = 0; 
+        vector<int> res; //find the most appeared numbers, different
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == m1) 
+                ++c1;
+            else if (nums[i] == m2)
+                ++c2;
+            else if (c1 == 0) {
+                m1 = nums[i];
+                c1 = 1;
+            }
+            else if (c2 == 0) {
+                m2 = nums[i];
+                c2 = 1;
+            }
+            else {
+                --c1;
+                --c2;
+            }
+        }
+        c1 = 0, c2 = 0; //get actual count of c1 and c2
+        for (auto v : nums) {
+            if (v == m1)
+                ++c1;
+            else if (v == m2)
+                ++c2;
+        }
+        if (c1 > nums.size()/3) res.push_back(m1);
+        if (c2 > nums.size()/3) res.push_back(m2);
+        
+        return res;
+    }
+};
+//majority 1 中假定众数一定存在了
+//需要两遍遍历，第一遍历找出两个候选众数，第二遍遍历重新投票验证这两个候选众//数是否为众数即可
