eb8c0ca589278cd6f3e342ec441ef2248b5d3065
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-16 18:29:09 -0700

    Create 213.House_Robber_II.cpp

---
+++ src/213.House_Robber_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1)
+            return n == 1 ? nums[0] : 0;
+        return max(rob(nums, 1, n - 1), rob(nums, 0, n - 2));
+    }
+    int rob(vector<int>& nums, int left, int right) {
+        if (right - left <= 1)  return nums[left]; //only one item
+        vector<int> dp(right + 1, 0);
+        dp[left] = nums[left];
+        dp[left + 1] = max(nums[left], nums[left + 1]);
+        for (int i = left + 2; i <= right; ++i)
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        return dp[right];
+    }
+};
+
+//如果我们把第一家和最后一家分别去掉，各算一遍能抢的最大值，然后比较两个值取其中较大的一个即为所求。
