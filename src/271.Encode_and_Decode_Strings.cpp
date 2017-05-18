1767e0b474f96bb1c187612de2929e1a7d317682
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-17 18:48:51 -0700

    Create 271.Encode_and_Decode_Strings.cpp

---
+++ src/271.Encode_and_Decode_Strings.cpp
@@ -0,0 +1,28 @@
+class Codec {
+public:
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string res;
+        for (auto s : strs)
+            res += to_string(s.length()) + "#" + s; // +=, not push_back
+        return res;
+    }
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> strs;
+        size_t last = -1;
+        size_t found = s.find_first_of("#");
+        while (found != string::npos) {
+            int len = stoi(s.substr(last + 1, found - last - 1));
+            string str = s.substr(found + 1, len);
+            last = found + len;
+            found = s.find_first_of("#", last + 1);
+            strs.push_back(str);
+        }
+        return strs;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.decode(codec.encode(strs));
