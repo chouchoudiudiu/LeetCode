4d4d142285761b7cef9a7fb915a550e8bb6c1ad8
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-18 05:35:43 -0700

    Create 306.Additive_Number.cpp

---
+++ src/306.Additive_Number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isAdditiveNumber(string num) {
+        int n = num.size();
+        for (int i = 1; i <= n/2; ++i)
+            for (int j = 1; j <= (n - i)/2; ++j) {
+                if (eval(num.substr(0, i), num.substr(i, j), num.substr(i + j)))
+                    return true;
+            }
+        return false;
+    }
+    bool eval(string s1, string s2, string s3) {
+        if ((s1.size() > 1 && s1[0] == '0') || (s2.size() > 1 && s2[0] == '0') ) //leading 0
+            return false;
+        string sum = add(s1, s2);
+        if (sum == s3) //已经找到
+            return true;
+        if ((sum.size() >= s3.size()) || (sum != s3.substr(0, sum.size())))
+            return false;//长度超过剩余，长度相等值却不等，和与s3前几位不等
+        
+        return eval(s2, sum, s3.substr(sum.size()));
+    }
+    //add is faster than stoll + stoll?
+    string add(string n1, string n2) {
+        string res;
+        int i = n1.size() - 1, j = n2.size() - 1, carry  = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry + (i >= 0 ? (n1[i--] - '0') : 0) + (j >= 0 ? (n2[j--] - '0') : 0);
+            res.push_back(sum%10 + '0');
+            carry = sum/10;
+        }
+        if (carry) res.push_back(carry + '0');
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+//"199110199" note: very slow! string sum = to_string(stoll(s1) + stoll(s2));
