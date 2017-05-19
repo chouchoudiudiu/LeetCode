a00ceb1777c0594b3d89ede04e477b0b549f5b12
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-19 08:42:18 -0700

    Create 345.Reverse_Vowels_of_a_String.cpp

---
+++ src/345.Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            l = s.find_first_of("aeiouAEIOU", l);
+            r = s.find_last_of("aeiouAEIOU", r);
+            if (l < r)
+                swap(s[l++], s[r--]);
+        }
+        return s;
+    }
+};
