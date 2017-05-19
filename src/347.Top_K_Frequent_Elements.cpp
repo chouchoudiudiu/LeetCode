c8cad30bd703ba431d12a26bcbda24b74f3a0b77
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-19 13:08:15 -0700

    Create 347.Top_K_Frequent_Elements.cpp

---
+++ src/347.Top_K_Frequent_Elements.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        for (auto v : nums) {
+            ++m[v];
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //min heap
+        for (auto v : m) {
+            pq.push({v.second, v.first}); //count, value
+            if (pq.size() > k) 
+                pq.pop();//pop the smallest one
+        }
+        while (!pq.empty()) {
+            res.push_back(pq.top().second); //actual value
+            pq.pop();
+        }
+        
+        return vector<int>(res.rbegin(), res.rend());
+    }
+};
+
+/* 无法保证是按顺序输出的,第一第二第三
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        for (auto v : nums) {
+            ++m[v];
+        }
+        priority_queue<pair<int, int>> pq; //maxheap, pair default sort by first
+        for (auto v : m) {
+            pq.push({v.second, v.first}); //count, value
+            if (pq.size() > m.size() - k) { //最多pop出m.size() - k次
+                res.push_back(pq.top().second);
+                pq.pop();
+            }
+        }
+        
+        return res;
+    }
+};
+*/
+
+//bucket sort?
