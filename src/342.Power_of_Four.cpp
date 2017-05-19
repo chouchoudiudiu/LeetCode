e46315f95449f04b32a9bc5235314c84a4e9a2ee
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-19 08:39:17 -0700

    Update 342.Power_of_Four.cpp

--- src/342.Power_of_Four.cpp
+++ src/342.Power_of_Four.cpp
@@ -9,3 +9,22 @@ public:
         return !(n & (n - 1));
     }
 };
+
+
+///////////////////////////////
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num <= 0)
+            return false;
+        if(num == 1)
+            return true;
+        while(num > 4)
+        {
+            if(num %4 != 0)
+                return false;
+            num /= 4;
+        }
+        return num%4 == 0;
+    }
+};
