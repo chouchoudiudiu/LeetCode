7b563e6f3dae7500eb6b5feac91e3ff7418d0848
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-21 11:34:46 -0700

    Create 383.Ransom_Note.cpp

---
+++ src/383.Ransom_Note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int mag[256] = {0};
+        int note[256] = {0};
+        for (auto c : magazine) 
+            ++mag[c];
+        for (auto c : ransomNote) {
+            if (--mag[c] < 0)
+                return false;
+        }
+        return true;
+    }
+};
