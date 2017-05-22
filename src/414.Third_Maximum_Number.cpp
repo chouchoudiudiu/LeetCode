8d69d324816372981a67cbed27f9f127e7fdffd6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-21 19:59:30 -0700

    Create 414.Third_Maximum_Number.cpp

---
+++ src/414.Third_Maximum_Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+        for (auto v : nums) {
+            if(v == first || v == second || v == third)
+                continue;
+            if (v > first) {
+                third = second;
+                second = first;
+                first = v;
+            }
+            else if (v > second) {
+                third = second;
+                second = v;
+            }
+            else if (v > third) 
+                third = v;
+        }
+        return (third != LONG_MIN  && third != second) ? third : first;
+    }
+};
+
+//[1,2,2,5,3,5]
+//[1,2,-2147483648]
