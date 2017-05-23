38ab5db615e03360fbb7b7edcf291460ae66bf0d
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-22 22:44:35 -0700

    Create 442.Find_All_Duplicates_in_an_Array.cpp

---
+++ src/442.Find_All_Duplicates_in_an_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] < 0)
+                res.push_back(idx + 1);
+            nums[idx] = -nums[idx];
+        }
+        return res;
+    }
+};
+
+//对于每个nums[i]，我们将其对应的nums[nums[i] - 1]取相反数，如果其已经是负数了，说明之前存在过
