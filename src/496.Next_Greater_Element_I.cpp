5da568d8efef93246e6fa95b07abe0e54c81b3ae
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-23 15:47:13 -0700

    Update 496.Next_Greater_Element_I.cpp

--- src/496.Next_Greater_Element_I.cpp
+++ src/496.Next_Greater_Element_I.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        if(findNums.empty() || nums.empty()) 
+            return {};
+        int n = findNums.size();
+        vector<int> res(n, -1);
+        for (int i = 0; i < findNums.size(); ++i) {
+            bool found = false;
+            for (auto num : nums) {
+                if (num == findNums[i])
+                    found = true;
+                if (found && num > findNums[i]) {
+                    res[i] = num;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        if(findNums.empty() || nums.empty()) 
+            return {};
+        int n = findNums.size();
+        vector<int> res(n, -1);
+        unordered_map<int, int> m; // value in second array, index in second array
+        for (int i = 0; i < nums.size(); ++i) 
+            m[nums[i]] = i;
+        for (int i = 0; i < findNums.size(); ++i) {
+            for (int j = m[findNums[i]] + 1; j < nums.size(); ++j) {
+                if (nums[j] > findNums[i]) {
+                    res[i] = nums[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
         vector<int> res;
         stack<int> stk;
         unordered_map<int, int> m; //in array 2, 每个数字右边第一个比它大的数的值
