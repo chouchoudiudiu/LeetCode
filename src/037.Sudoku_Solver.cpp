3cf4fa1585315fe9f949cf3b1b6c4625dffe07a5
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-29 18:14:34 -0700

    Update 037.Sudoku_Solver.cpp

--- src/037.Sudoku_Solver.cpp
+++ src/037.Sudoku_Solver.cpp
@@ -1,59 +1,36 @@
 class Solution {
 public:
-bool isValid(vector<vector<char> > &board, int x, int y)
-    {
-        bool rows[9] = {false}, cols[9] = {false}, blocks[9] = {false};
-        for (int i = 0; i < 9; ++i) {
-            if (board[x][i] != '.') {
-                int val = board[x][i] - '1';
-                if (rows[val])
+    bool isValid(vector<vector<char>>& board, int x, int y) {
+        for(int row = 0; row < 9; ++row) {
+            if(row != x && board[row][y] == board[x][y])
+                return false;
+        }
+        for(int col = 0; col < 9; ++col) {
+            if(col != y && board[x][col] == board[x][y])
+                return false;
+        }
+        for(int row = x/3*3; row < x/3*3 + 3; ++row)
+            for (int col = y/3*3; col < y/3*3 + 3; ++col) {
+                if ((row != x || col != y) && board[row][col] == board[x][y])
                     return false;
-                rows[val] = true;
             }
-        }
-        for (int i = 0; i < 9; ++i) {
-            if (board[i][y] != '.') {
-                int val = board[i][y] - '1';
-                if (cols[val])
-                    return false;
-                cols[val] = true;
-            }
-        }
-        for (int i = 0; i < 3; ++i)
-            for (int j = 0; j < 3; ++j) {
-                if (board[x/3*3 + i][y/3*3 + j] != '.') {
-                    int val = board[x/3*3 + i][y/3*3 + j] - '1';
-                    if (blocks[val])
-                        return false;
-                    blocks[val] = true;
-            }
-        }
         return true;
     }
-
-    
-    bool solveSudokuWrapper(vector<vector<char>> &board, int curr) {
+    bool solveSudokuHelper(vector<vector<char>>& board, int curr) {
         if (curr == 81)
             return true;
         int x = curr/9, y = curr%9;
         if (board[x][y] != '.')
-            return solveSudokuWrapper(board, curr + 1);
-        else {
-            for (int i = 1; i <= 9; ++i) {
-                board[x][y] = i + '0';
-                if (isValid(board, x, y)) {
-                    if (solveSudokuWrapper(board, curr + 1)) //keep going
-                        return true;
-                }
-                board[x][y] = '.'; //reset for every i
-            }
-            return false; //none works
+            return solveSudokuHelper(board, curr + 1);
+        for (int i = 1; i <= 9; ++i) {
+            board[x][y] = i + '0';
+            if (isValid(board, x, y) && solveSudokuHelper(board, curr + 1))
+                return true;
+            board[x][y] = '.'; 
         }
+        return false;
     }
-    
     void solveSudoku(vector<vector<char>>& board) {
-        solveSudokuWrapper(board, 0);
+        solveSudokuHelper(board, 0);
     }
 };
-/*
-["51.748632","783652419","426139875","357986241","264317598","198524367","975863124","832491756","6412759.3"]*/
