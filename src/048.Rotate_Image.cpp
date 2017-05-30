3fac1c61165167c8e8961fff4001cf8ddd14ccbb
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-30 11:50:20 -0700

    Update 048.Rotate_Image.cpp

--- src/048.Rotate_Image.cpp
+++ src/048.Rotate_Image.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for (int i = 0; i < n/2; ++i)
-            for (int j = i; j < n - 1 - i; ++j)
-            {
-                int tmp = matrix[i][j];
-                cout<<tmp<<endl;
-                matrix[i][j] = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]; 
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = tmp;
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < matrix.size(); ++i) 
+            for (int j = i + 1; j < matrix[i].size(); ++j) { //8, 9; 6
+                swap(matrix[i][j], matrix[j][i]);
             }
     }
 };
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
+
+/*
+ * anticlockwise rotate
+ * first reverse left to right, then swap the symmetry
+ * 1 2 3     3 2 1     3 6 9
+ * 4 5 6  => 6 5 4  => 2 5 8
+ * 7 8 9     9 8 7     1 4 7
+ 
+for (int i = 0; i < matrix.size(); ++i)
+    reverse(matrix[i].begin(), matrix[i].end());
+for (int i = 0; i < matrix.size(); ++i) 
+    for (int j = i + 1; j < matrix[i].size(); ++j) { //8, 9; 6
+        swap(matrix[i][j], matrix[j][i]);
+    }
+*/
