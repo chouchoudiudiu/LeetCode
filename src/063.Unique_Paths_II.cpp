d8dcd4fd16ec21c61e4a269e85b1538d30076336
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-05-31 17:18:23 -0700

    Update 063.Unique_Paths_II.cpp

--- src/063.Unique_Paths_II.cpp
+++ src/063.Unique_Paths_II.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles (vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid.empty())   
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+         if (obstacleGrid.empty())
             return 0;
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-        vector<vector<int>> mat(m, vector<int>(n, 0));
-        if (obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
-        mat[m - 1][n - 1] = 1;//initialization!
-        for (int i = m - 2; i >= 0; --i)
-            mat[i][n - 1] = obstacleGrid[i][n - 1] == 1 ? 0 : mat[i + 1][n - 1];
-        for (int j = n - 2; j >= 0; --j)
-            mat[m - 1][j] = obstacleGrid[m - 1][j] == 1 ? 0 : mat[m - 1][j + 1];
-        
-        for (int i = m - 2; i >= 0; --i)
-            for (int j = n - 2; j >= 0; --j) {
-                mat[i][j] = obstacleGrid[i][j] == 1? 0:mat[i + 1][j] + mat[i][j + 1];
-        }
-    
-        return mat[0][0];
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j) {
+                if (obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else if (i == 0 && j == 0)
+                    dp[i][j] = 1;
+                else if (i > 0 && j == 0)
+                    dp[i][j] = dp[i - 1][j];
+                else if (i == 0 && j > 0)
+                    dp[i][j] = dp[i][j - 1];
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        return dp[m - 1][n - 1];
     }
-
 };
