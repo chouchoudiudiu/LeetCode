9de72d05ac8b058e43d823dce7fbe85d5e23d536
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-12 17:32:06 -0700

    Create 245.Shortest_Word_Distance_III.cpp

--- src/245.Shortest_Word_Distance_III.cpp
+++ src/245.Shortest_Word_Distance_III.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int shortestWordDistance(vector<string>& words, string word1, string word2) {
-        int pos1 = -1, pos2 = -1, pos3 = -1, minD = INT_MAX;
-        for (int i = 0; i < words.size(); ++i) {
-            if (word1 == words[i])
+        int pos1 = -1, pos2 = -1, minD = INT_MAX;
+        for(int i = 0; i < words.size(); ++i) {
+            if(words[i] == word1)
                 pos1 = i;
-            if (word1 != word2) {
-                if(word2 == words[i])
+            if(word1 != word2) {
+                if(words[i] == word2)
                     pos2 = i;
-                if (pos1 != -1 && pos2 != -1) 
+                if(pos1 != -1 && pos2 != -1)
                     minD = min(minD, abs(pos1 - pos2));
             }
             else { //word1 == word2
                 if(words[i] == word1) {
-                    if(pos3 != -1) //not the first time meet
-                        minD = min(minD, i - pos3);
-                    pos3 = i; //last time it sees a duplicate
+                    if(pos2 != i && pos1 != -1 && pos2 != -1) //not the first time meet
+                        minD = min(minD, pos1 - pos2); //pos1 is fresher
+                    pos2 = i;   //last time it sees a duplicate
                 }
             }
         }
