5805e3619fadbb307505816bcbe45ea236126ed6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-13 14:41:13 -0700

    Create 268.Missing_Number.cpp

--- src/268.Missing_Number.cpp
+++ src/268.Missing_Number.cpp
@@ -10,3 +10,14 @@ public:
         return sumExpected - sum;
     }
 };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); ++i) 
+            res ^= (i + 1)^nums[i];
+        
+        return res;
+    }
+};
