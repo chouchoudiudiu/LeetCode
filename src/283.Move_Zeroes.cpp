f802456ff94f6f0fd3215a57be1306d923f71eb7
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-14 13:55:54 -0700

    Create 283.Move_Zeroes.cpp

--- src/283.Move_Zeroes.cpp
+++ src/283.Move_Zeroes.cpp
@@ -8,3 +8,16 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0;
+        for(int r = 0; r < nums.size(); ++r) {
+            if(nums[r] != 0)
+                nums[l++] = nums[r];
+        }
+        for(int i = l; i < nums.size(); ++i)
+            nums[i] = 0;
+    }
+};
