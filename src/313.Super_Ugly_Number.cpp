27ac48f527e69496b62e0d139a57360f536c218c
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-15 11:26:40 -0700

    Update 313.Super_Ugly_Number.cpp

--- src/313.Super_Ugly_Number.cpp
+++ src/313.Super_Ugly_Number.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        if (n <= 0 || primes.empty())
+        if(n <= 0 || primes.empty())
             return 0;
-        int k = primes.size();
         vector<int> uglyNumbers(n, 1);
-        vector<int> p(k, 0); //下一个应该乘以的ugly number 的index
-        vector<int> ugly(k);
+        int sz = primes.size();
+        vector<int> p(sz, 0); //下一个应该乘以的ugly number 的index
         int i = 1;
-        while (i < n) {
-            int minIdx = 0;
-            for (int j = 0; j < k; ++j) {
-                ugly[j] = uglyNumbers[p[j]] * primes[j];
-                if (ugly[j] < ugly[minIdx])
-                    minIdx = j;
+        while(i < n) {
+            int minVal = INT_MAX;
+            for(int j = 0; j < sz; ++j) 
+                minVal = min(minVal, uglyNumbers[p[j]]*primes[j]);
+             
+            for(int j = 0; j < sz; ++j) {
+                if(minVal == uglyNumbers[p[j]]*primes[j])
+                    ++p[j];
             }
-            for (int j = 0; j < k; ++j) {
-                if (ugly[j] == ugly[minIdx])
-                    p[j]++;
-            }
-            uglyNumbers[i++] = ugly[minIdx];
+            uglyNumbers[i++] = minVal;
         }
         
         return uglyNumbers[n - 1];
