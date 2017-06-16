cf3a6b96204ada97a85dce81c3dc224b3c3ad323
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-15 17:29:03 -0700

    Update 312.Burst_Balloons.cpp

--- src/312.Burst_Balloons.cpp
+++ src/312.Burst_Balloons.cpp
@@ -6,7 +6,7 @@ public:
         nums.push_back(1);
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0)); //nums.size(), not N 打爆区间[i,j]中的所有气球能得到的最多金币
         //find which balloon to burst LAST will give us the most coins for that subinterval
-        for (int len = 1; len <= N; ++len)
+        for (int len = 1; len <= N; ++len) //burst 1 个balloon 到所有N个
             for (int left = 1; left <= N - len + 1; ++left) {
 	            int right = left + len - 1;
 	            for (int k = left; k <= right; ++k) {
