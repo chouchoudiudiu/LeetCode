34b17faee83ccd0bc8c500458049a0c55284592e
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-22 07:45:04 -0700

    Update 357.Count_Numbers_with_Unique_Digits.cpp

--- src/357.Count_Numbers_with_Unique_Digits.cpp
+++ src/357.Count_Numbers_with_Unique_Digits.cpp
@@ -13,3 +13,4 @@ public:
 };
 //第一位只能从1-9中选择，后面的几位由10-(i-1)确定
 //the first i - 1 location takes up those numbers, 10 - (i - 1) numbers left
+//通项公式为f(k) = 9 * 9 * 8 * ... (9 - k + 2)，那么我们就可以根据n的大小，把[1, n]区间位数通过通项公式算出来累加起来即可
