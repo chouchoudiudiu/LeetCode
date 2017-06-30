95aa40f110d306d070447c770cc3c647939da5d8
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-06-30 13:10:17 -0700

    Update 432.All_O_one_Data_Structure.cpp

--- src/432.All_O_one_Data_Structure.cpp
+++ src/432.All_O_one_Data_Structure.cpp
@@ -5,28 +5,32 @@ public:
         
     }
     
-       /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
+    /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     void inc(string key) {
-        if (!m.count(key)) {
-            if (buckets.empty() || buckets.back().val != 1) {
+        if(!m.count(key)) {
+            if(buckets.empty() || buckets.back().freq != 1) {
                 auto newBucket = buckets.insert(buckets.end(), {1, {key}});
                 m[key] = newBucket;
-            } else {
+            }
+            else {
                 auto newBucket = --buckets.end();
                 newBucket->keys.insert(key);
                 m[key] = newBucket;
             }
-        } else {
+        }
+        else {
             auto curBucket = m[key], lastBucket = (--m[key]);
-            if (lastBucket == buckets.end() || lastBucket->val != curBucket->val + 1) {
-                auto newBucket = buckets.insert(curBucket, {curBucket->val + 1, {key}});
+            if(lastBucket == buckets.end() || lastBucket->freq != curBucket->freq + 1) {
+                auto newBucket = buckets.insert(curBucket, {curBucket->freq + 1, {key}});
                 m[key] = newBucket;
-            } else {
+            }
+            else {
                 lastBucket->keys.insert(key);
                 m[key] = lastBucket;
             }
             curBucket->keys.erase(key);
-            if (curBucket->keys.empty()) buckets.erase(curBucket);
+            if(curBucket->keys.empty())
+                buckets.erase(curBucket);
         }
     }
     
@@ -34,15 +38,15 @@ public:
     void dec(string key) {
         if (!m.count(key)) return;
         auto curBucket = m[key];
-        if (curBucket->val == 1) {
+        if (curBucket->freq == 1) {
             curBucket->keys.erase(key);
             if (curBucket->keys.empty()) buckets.erase(curBucket);
             m.erase(key);
             return;
         }
         auto nextBucket = ++m[key];
-        if (nextBucket == buckets.end() || nextBucket->val != curBucket->val - 1) {
-            auto newBucket = buckets.insert(nextBucket, {curBucket->val - 1, {key}});
+        if (nextBucket == buckets.end() || nextBucket->freq != curBucket->freq - 1) {
+            auto newBucket = buckets.insert(nextBucket, {curBucket->freq - 1, {key}});
             m[key] = newBucket;
         } else {
             nextBucket->keys.insert(key);
@@ -61,14 +65,14 @@ public:
     string getMinKey() {
         return buckets.empty() ? "" : *(buckets.rbegin()->keys.begin()); //or end()?
     }
-private: 
-    struct Bucket
-    {
-        int val; //出现次数
-        unordered_set<string> keys; //存次数相同的所有key值
+    
+private:
+    struct Bucket {
+        int freq; // 出现次数
+        unordered_set<string> keys; //次数相同的所有key值
     };
-    list<Bucket> buckets; //按value大小从大到小排列
-    unordered_map<string, list<Bucket>::iterator> m; //key value 建立key和list中位置迭代器之间的映射
+    list<Bucket> buckets; //按freq从大到小排列
+    unordered_map<string, list<Bucket>::iterator> m; //key value
 };
 
 /**
