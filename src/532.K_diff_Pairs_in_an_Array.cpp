e66ad63495e56636b1f3d28d0df6a85319f4eaa5
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-05 10:44:19 -0700

    Create 532.K_diff_Pairs_in_an_Array.cpp

---
+++ src/532.K_diff_Pairs_in_an_Array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        int res = 0;
+        for(auto v : nums)
+            ++m[v];
+        for(auto a : m) {
+            if(k == 0 && a.second > 1)
+                ++res;
+            if(k > 0 && m.count(a.first + k))
+                ++res;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        if (k < 0) return 0;
+        sort(nums.begin(), nums.end());
+        int cnt = 0, i = 0, n = nums.size(), j = n - 1;
+        while (i <= j) { //to enter else
+            if (nums[j] - nums[i] > k)
+                --j;
+            else { //== <可忽略
+                if(i != j && nums[j] - nums[i] == k)
+                    ++cnt;
+                while (i < n - 1 && nums[i] == nums[i + 1])
+                    ++i;
+                ++i;
+                j = n - 1;
+            }
+        }
+        return cnt;
+    }
+};
+/*
+[6,2,9,3,9,6,7,7,6,4]
+3
+*/
