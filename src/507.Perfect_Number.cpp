a36cda307e06e30d3aba3ba54fded9c3d3d8dee5
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-05 19:31:54 -0700

    Create 507.Perfect_Number.cpp

---
+++ src/507.Perfect_Number.cpp
@@ -0,0 +1,19 @@
+ 
+Add to List
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num <= 1)
+            return false;
+        int sum = 1;
+        for(int i = 2; i <= sqrt(num); ++i) {
+            if(num%i == 0) {
+                sum += i;
+                sum += num/i;
+            }
+        }
+        return sum == num;
+    }
+};
+
+//1 is not true
