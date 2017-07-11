ef893ce0929cc516788903325365f5193de8ed08
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-10 20:17:06 -0700

    Create 524.Longest_Word_in_Dictionary_through_Deleting.cpp

---
+++ src/524.Longest_Word_in_Dictionary_through_Deleting.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        int maxLen = 0;
+        string res;
+        for(auto dict : d) {
+            int i = 0, j = 0, n = dict.length();
+            while(i < s.length() && j < n) {
+                if(s[i] == dict[j]) {
+                    ++i;
+                    ++j;
+                }
+                else
+                    ++i;
+            }
+            if(j == n) {
+                if(n >= maxLen) {
+                    if(maxLen == n) 
+                        res = dict < res ? dict : res;
+                    else 
+                        res = dict;
+                    maxLen = max(maxLen, n);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//goal Time Complexity: O(nk), where n is the length of string s and k is the number of words in the dictionary.
