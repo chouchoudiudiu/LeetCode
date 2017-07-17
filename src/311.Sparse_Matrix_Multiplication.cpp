66869dfe05eea125bce198b5f74a0cf1f59c148e
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-17 09:51:25 -0700

    Update 311.Sparse_Matrix_Multiplication.cpp

--- src/311.Sparse_Matrix_Multiplication.cpp
+++ src/311.Sparse_Matrix_Multiplication.cpp
@@ -36,3 +36,22 @@ public:
         return res;
     }
 };
+
+////////////math
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        if(A.empty() || B.empty())
+            return {};
+        int m = A.size(), n = A[0].size(), p = B[0].size(); 
+        vector<vector<int>> res(m, vector<int>(p, 0));
+        for(int i = 0; i < m; ++i)
+            for(int k = 0; k < n; ++k) {
+                if(A[i][k] != 0) {
+                    for(int j = 0; j < p; ++j)
+                        res[i][j] += A[i][k]*B[k][j];
+                }
+            }
+        return res;
+    }
+};
