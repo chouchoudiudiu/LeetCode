5ccce63f9c417961a99e34f33991dfef729fb4fc
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-17 23:58:40 -0700

    Update 279.Perfect_Squares.cpp

--- src/279.Perfect_Squares.cpp
+++ src/279.Perfect_Squares.cpp
@@ -12,3 +12,32 @@ public:
         return dp[n];
     }
 };
+
+//bfs
+class Solution {
+public:
+    int numSquares(int n) {
+        int steps = 0;
+        queue<int> q;
+        q.push(n);
+        vector<bool> visited(n, false);
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; ++i) {
+                int top = q.front();
+                q.pop();
+                if(top == 0)
+                    return steps;
+                for(int j = 1; j <= sqrt(top); ++j) {
+                    int val = top - j*j;
+                    if(!visited[val]) {
+                        q.push(val);
+                        visited[val] = true;
+                    }
+                }
+            }
+            ++steps;
+        }
+        return steps;
+    }
+};
