f79e49dd1d2e0f20bc91e49d7454ceb816188b56
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-27 09:45:43 -0700

    Update 267.Palindrome_Permutation_II.cpp

--- src/267.Palindrome_Permutation_II.cpp
+++ src/267.Palindrome_Permutation_II.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     vector<string> generatePalindromes(string s) {
         vector<string> res;
-        string half = "", mid = "";
         unordered_map<char, int> m;
-        for (auto c : s) ++m[c];
-        for (auto v : m) {
-            if (v.second % 2 == 1)
-                mid += v.first;
-            half += string(v.second/2, v.first);
-            if (mid.length() > 1)
-                return {};//not able to generate palindromes
+        string half = "", mid = "";
+        for(auto c : s)
+            ++m[c];
+        bool odd = false;
+        for(auto v : m) {
+            if(v.second%2 == 1)
+                mid += v.first; //直接接上单次出现的字符
+            half += string(v.second/2, v.first); //相同的字符放在一起，/2
+            if(mid.length() > 1)
+                return {}; //not able to generate palindromes
         }
-        permute(half, mid, 0, res); //no need to sort like permutation2 since it is like sorted/grouped
+        helper(half, mid, res, 0); //通过swap改变half的值，不需要另外写out
         return res;
     }
-    void permute(string& half, string mid, int start, vector<string> &res) {
-        if (start >= half.length()) {
+    void helper(string half, string mid, vector<string>& res, int i) {
+        if(i == half.size()) {
             res.push_back(half + mid + string(half.rbegin(), half.rend()));
-        }
+        }   
         unordered_set<char> used;
-        for (int i = start; i < half.length(); ++i) {
-            if (used.find(half[i]) == used.end()) {
-                swap(half[i], half[start]);
-                permute(half, mid, start + 1, res);
-                swap(half[i], half[start]);
-                used.insert(half[i]);
+        for(int j = i; j < half.size(); ++j) {
+            if(used.find(half[j]) == used.end()) { //same as permutation ii, 一样的字母swap没有意义
+                swap(half[j], half[i]);
+                helper(half, mid, res, i + 1);
+                swap(half[j], half[i]);
+                used.insert(half[j]);
             }
         }
     }
