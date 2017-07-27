960a1a81ab1bb90cbfbcef65c9ac8f03ba581fb6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-26 21:29:55 -0700

    Update 131.Palindrome_Partitioning.cpp

--- src/131.Palindrome_Partitioning.cpp
+++ src/131.Palindrome_Partitioning.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-//similar to word break ii
+    //very similar to word break ii
     bool isPalindrome(string s) {
         int l = 0, r = s.length() - 1;
-        while (l <= r) {
-            if (s[l++] != s[r--]) 
+        while(l < r) {
+            if(s[l++] != s[r--])
                 return false;
         }
         return true;
     }
-    void dfs(string s, vector<string>& output, int index, vector<bool>& dp, vector<vector<string>>& res) {
-        if (index == s.length()) {
-            res.push_back(output);
+    void helper(string s, vector<string>& out, vector<vector<string>>& res, int idx, vector<bool>& dp) {
+        if(idx == s.length()) {
+            res.push_back(out);
             return;
         }
-        for (int i = index; i < s.length(); ++i) {
-            string tmp = s.substr(index, i - index + 1);
-            if (isPalindrome(tmp) && dp[i + 1]) {
-                output.push_back(tmp);
+        for(int i = idx; i < s.length(); ++i) {
+            string tmp = s.substr(idx, i - idx + 1);
+            if(isPalindrome(tmp) && dp[i]) {
                 int size = res.size();
-                dfs(s, output, i + 1, dp, res);
-                if (size == res.size()) //no solution found
-                    dp[i + 1] = false;
-                output.pop_back();
+                out.push_back(tmp);
+                helper(s, out, res, i + 1, dp);
+                if(res.size() == size) //no solution found
+                    dp[i] = false;
+                out.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
-        vector<string> output;
-        vector<bool> dp (s.length() + 1, true); //initialized as true
-        dfs(s, output, 0, dp, res);
+        vector<bool> dp(s.length(), true);
+        vector<string> out;
+        helper(s, out, res, 0, dp);
         return res;
     }
 };
