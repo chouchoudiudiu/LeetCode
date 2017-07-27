a0b6eadbde5a8e64944d4b2dd5ba8d8f66d7d483
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-26 17:08:18 -0700

    Update 010.Regular_Expression_Matching.cpp

--- src/010.Regular_Expression_Matching.cpp
+++ src/010.Regular_Expression_Matching.cpp
@@ -12,7 +12,7 @@ public:
             ++s;
         }
         return isMatch(s, p + 2); //not match the first char; such as a... at*
-    }
+    } //s = 'ta' p = 'm*ta' or s = 'ttttta' p = m*ta bad but p = m*t*a works
     bool isMatch(string s, string p) {
         return isMatch(s.c_str(), p.c_str());       
     }
