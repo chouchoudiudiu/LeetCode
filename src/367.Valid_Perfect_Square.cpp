31f0ef890d2ddafed874c9265138ecf036cac30f
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-29 11:28:52 -0700

    Update 367.Valid_Perfect_Square.cpp

--- src/367.Valid_Perfect_Square.cpp
+++ src/367.Valid_Perfect_Square.cpp
@@ -11,3 +11,21 @@ public:
 };
 
 //2147483647 TLE
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1, high = num;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(mid*mid == num)
+                return true;
+            else if(mid < num/mid)
+                low = mid + 1;
+            else 
+                high = mid - 1;
+        }
+        return false;
+    }
+};
+
+//binary search to find sqrt(x)
