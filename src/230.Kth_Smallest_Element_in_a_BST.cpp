59420e3407afcc0032a5395ef604a435a7f43409
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-29 10:58:55 -0700

    Update 230.Kth_Smallest_Element_in_a_BST.cpp

--- src/230.Kth_Smallest_Element_in_a_BST.cpp
+++ src/230.Kth_Smallest_Element_in_a_BST.cpp
@@ -25,3 +25,34 @@ public:
         return ret;
     }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        int cnt = countNodes(root->left);
+        if(cnt >= k)
+            return kthSmallest(root->left, k);
+        else if(k > cnt + 1)
+            return kthSmallest(root->right, k - cnt - 1);
+        return root->val; //k == cnt + 1
+    }
+    
+    int countNodes(TreeNode* root) {
+        if(!root)
+            return 0;
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
+
+//followup?
