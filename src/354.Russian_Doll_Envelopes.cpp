d96e66c0edf029bd09b758bc6efda1750ee3aed2
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-30 05:07:28 -0700

    Update 354.Russian_Doll_Envelopes.cpp

--- src/354.Russian_Doll_Envelopes.cpp
+++ src/354.Russian_Doll_Envelopes.cpp
@@ -3,17 +3,16 @@ public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end());
         int n = envelopes.size();
-        int maxSize = 1; //at least one self
         vector<int> dp(n, 1);
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
+        int maxSize = 0; //at least one self
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < i; ++j) {
                 if (envelopes[i].first > envelopes[j].first && envelopes[i].second > envelopes[j].second)
                     dp[i] = max(dp[i], dp[j] + 1);
-            }    
+            }
             maxSize = max(maxSize, dp[i]);
         }
-        
-        return n == 0 ? 0 : maxSize; 
+        return maxSize;
     }
 };
 
