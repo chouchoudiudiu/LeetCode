4a1d42c5ac40b9c5893c9c6173de6e7faa981b19
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-30 05:34:38 -0700

    Update 354.Russian_Doll_Envelopes.cpp

--- src/354.Russian_Doll_Envelopes.cpp
+++ src/354.Russian_Doll_Envelopes.cpp
@@ -17,3 +17,25 @@ public:
 };
 
 //optimize
+class Solution {
+public:
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
+            return (a.first == b.first) ? a.second > b.second : a.first < b.first;});
+        vector<int> vec;
+        for(auto v : envelopes) {
+            auto pos = lower_bound(vec.begin(), vec.end(), v.second);
+            if(pos == vec.end())
+                vec.push_back(v.second);
+            else 
+                *pos = v.second;
+        }
+        return vec.size();
+    }
+};
+
+//完全一样的呢？
+//[[4,5],[6,7],[2,3]]
+//要考虑一下LIS中如果允许可以相等的情况，该如何改变？
+/*我们还可以使用二分查找法来优化速度，我们首先要做的还是给信封排序，但是这次排序和上面有些不同，信封的宽度还是从小到大排，但是宽度相等时，我们让高度大的在前面。那么现在问题就简化了成了找高度数字中的LIS，完全就和之前那道Longest Increasing Subsequence一样了
+*/
