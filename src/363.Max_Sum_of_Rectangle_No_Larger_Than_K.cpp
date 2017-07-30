20153f770d083c2c5e3b5286c19e225afb37292d
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-30 16:01:56 -0700

    Update 363.Max_Sum_of_Rectangle_No_Larger_Than_K.cpp

--- src/363.Max_Sum_of_Rectangle_No_Larger_Than_K.cpp
+++ src/363.Max_Sum_of_Rectangle_No_Larger_Than_K.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-//What if the number of rows is much larger than the number of columns?
-//space: O(row); time: O(col*col*row) or O(col*col*log(row)) ? 
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        if (matrix.empty()) return 0;
+        if(matrix.empty())
+            return 0;
         int m = matrix.size(), n = matrix[0].size();
         int res = INT_MIN; //might be negative
-        for (int l = 0; l < n; ++l) {
+        for(int l = 0; l < n; ++l) {
             vector<int> sums(m, 0);
-            for (int r = l; r < n; ++r) {
-                for (int i = 0; i < m; ++i)
+            for(int r = l;  r < n; ++r) {
+                for(int i = 0; i < m; ++i)
                     sums[i] += matrix[i][r];
                 set<int> cumSet;
-                cumSet.insert(0); //be careful!
-                int cumSum = 0, maxSumSubArray = INT_MIN;
-                for (auto s : sums) {
+                cumSet.insert(0); //be careful
+                int cumSum = 0, maxSumSubarray = INT_MIN;
+                for(auto s : sums) {
                     cumSum += s;
-                    auto it = cumSet.lower_bound(cumSum - k);//>=, binary search internally, 若找到=>中间一段<=k
-                    if (it != cumSet.end())
-                        maxSumSubArray = max(maxSumSubArray, cumSum - *it);//can be the same
+                    auto it = cumSet.lower_bound(cumSum - k); //>=, binary search internally, 若找到=>中间一段<=k
+                    if(it != cumSet.end())
+                        maxSumSubarray = max(maxSumSubarray, cumSum - *it);//can be the same
                     cumSet.insert(cumSum);
                 }
-                res = max(res, maxSumSubArray);
+                res = max(res, maxSumSubarray);
             }
         }
         return res;
     }
 };
-
-/*
-[[5,-4,-3,4],[-3,-4,4,5],[5,1,5,-4]]
-3
-*/
+//固定左边，移动右边，每一对[l, r], sum都先从上到下，依次从左到右加，得到的是每一行的sum
+//Maximum Sum Rectangular Submatrix in Matrix dynamic programming/2D kadane
+//What if the number of rows is much larger than the number of columns?
+//space: O(row); time: O(col*col*row) or O(col*col*log(row)) ? 
