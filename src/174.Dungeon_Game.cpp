7cb8b1ff48e693a37f63d596dad80f5806915089
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-30 10:37:43 -0700

    Update 174.Dungeon_Game.cpp

--- src/174.Dungeon_Game.cpp
+++ src/174.Dungeon_Game.cpp
@@ -5,7 +5,7 @@ public:
             return 0;
         int m = dungeon.size(), n = dungeon[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]);
+        dp[m - 1][n - 1] = max(1, 1 - dungeon[m - 1][n - 1]); //最后还要有1
         for (int i = m - 2; i >= 0; --i)
             dp[i][n - 1] = max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
         for (int j = n - 2; j >= 0; --j)
