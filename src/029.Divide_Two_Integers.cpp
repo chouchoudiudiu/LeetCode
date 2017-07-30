47f6f4eadd61eab72ba63c6d9db0ea550db1e5f7
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-30 05:01:04 -0700

    Update 029.Divide_Two_Integers.cpp

--- src/029.Divide_Two_Integers.cpp
+++ src/029.Divide_Two_Integers.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {// beichushu , chushu
-        if (divisor == 0 || (dividend == INT_MIN && divisor == -1))
+    int divide(int dividend, int divisor) { // beichushu , chushu
+        if(divisor == 0 || (dividend == INT_MIN && divisor == -1))
             return INT_MAX;
-        long long dend = abs((long long)dividend), dsor = abs((long long)divisor);
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+        long long dend = llabs(dividend);
+        long long dsor = llabs(divisor);
         int shift = 0, res = 0;
-        while (dsor << shift <= dend ) {
+        while(dsor << shift <= dend ) {
             ++shift;
+        } //final answer would be smaller than 2^shift
+        while(--shift >= 0) {
+           if(dsor << shift <= dend ) {
+               dend -= (dsor << shift);
+               res |= 1 <<shift;
+           }    
         }
-        while (--shift >= 0) {
-            if (dsor << shift <= dend) {
-                dend -= dsor << shift;
-                res |= 1 << shift;
-            }
-        }
-        if ((dividend < 0) ^ (divisor < 0))   
-            res = -res;
-        return res >= INT_MAX ? INT_MAX: res;
+        return sign == 1 ? res : -res;
     }
 };
