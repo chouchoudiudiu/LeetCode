91f2cb4b8b2cf21429dd7185cc649209b31d7cb3
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-07-29 23:28:28 -0700

    Update 275.H-Index_II.cpp

--- src/275.H-Index_II.cpp
+++ src/275.H-Index_II.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int low = 0, high = n - 1;
-        while (low <= high) {//出来时low > high, low更靠右，找不到刚好等于的，只能找靠右的
+        int low = 0, high = n - 1; 
+        while(low <= high) {//出来时low > high, low更靠右，找不到刚好等于的，只能找靠右的
             int mid = low + (high - low)/2;
-            if (citations[mid] < n - mid) //从右往左找到第一个citations[i] >= n - i的
+            if(citations[mid] >= n - mid) //从右往左找到第一个citations[i] >= n - i的
+                high = mid - 1;//keep trying to move left
+            else
                 low = mid + 1;
-            else if (citations[mid] > n - mid) //keep trying to move left
-                high = mid - 1;
-            else//==
-                return n - mid; //无法再往左，左边数变小，而n - i增加
         }
         return n - low;
     }
 };
+//无法再往左，左边数变小，而n - i增加
+//citations = [3, 0, 6, 1, 5] [0, 1, 3, 5, 6]
+//出来时low > high, low更靠右，找不到刚好等于的，只能找靠右的
+//其实就是search insert position...
