a3ec89b9c3c0620b61d9b817edb3f543ad53761f
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-01 12:20:18 -0700

    Update 253.Meeting_Rooms_II.cpp

--- src/253.Meeting_Rooms_II.cpp
+++ src/253.Meeting_Rooms_II.cpp
@@ -32,16 +32,6 @@ public:
     }
 };
 
-//[[2,11],[6,16],[11,16],[4,12]]
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
     int minMeetingRooms(vector<Interval>& intervals) {
