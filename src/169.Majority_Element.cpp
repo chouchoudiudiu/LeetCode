7f76333183d7c248c3947fd6fe1f10522d2bbd52
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-13 20:07:22 -0700

    Update 169.Majority_Element.cpp

--- src/169.Majority_Element.cpp
+++ src/169.Majority_Element.cpp
@@ -15,3 +15,20 @@ public:
         return majority;
     }
 };
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = nums[0], cnt = 1;
+        for(int i = 1; i < nums.size(); ++i) {
+            if(cnt == 0)
+                majority = nums[i];
+            if(nums[i] == majority)
+                ++cnt;
+            else
+                --cnt;
+        }
+        return majority;
+    }
+};
