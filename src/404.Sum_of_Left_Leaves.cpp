0161330ac36b55da0f1bb02695ab4ac8d836cb12
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-27 08:30:58 -0700

    Update 404.Sum_of_Left_Leaves.cpp

--- src/404.Sum_of_Left_Leaves.cpp
+++ src/404.Sum_of_Left_Leaves.cpp
@@ -23,3 +23,32 @@ public:
         return sum;
     }
 };
+
+/////////////////////////
+//BFS
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sumleft = 0;
+        if(!root)
+            return 0;
+        queue<pair<TreeNode*, bool>> q;
+        q.push({root, false});
+        while(!q.empty()) {
+            int sz = q.size();
+            for(int i = 0; i < sz; ++i) {
+                auto p = q.front();
+                q.pop();
+                if(!p.first->left && !p.first->right && p.second) //leaf, left, not necessarily the first one!
+                    sumleft += p.first->val;
+                if(p.first->left)
+                    q.push({p.first->left, true});
+                if(p.first->right)
+                    q.push({p.first->right, false});
+            }
+        }
+        return sumleft;
+    }
+};
+
+//[1] expected 0
