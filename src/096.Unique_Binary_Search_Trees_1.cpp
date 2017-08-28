e54eefb26b937cb32c8638aa5b3038a184efdb4e
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-27 22:08:04 -0700

    Update 096.Unique_Binary_Search_Trees_1.cpp

--- src/096.Unique_Binary_Search_Trees_1.cpp
+++ src/096.Unique_Binary_Search_Trees_1.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int numTrees(int n) {
-        if (n <= 1)
-            return n;
         vector<int> nums(n + 1, 0);
         nums[0] = 1;
         nums[1] = 1;
-        for (int i = 2; i <= n; ++i) {
-            int sum = 0;
-            for (int root = 1; root <= i; ++root) {
-                int left = nums[root - 1];
-                int right = nums[i - root];
-                sum += left*right;
+        for(int i = 2; i <= n; ++i) 
+            for(int root = 1; root <= i; ++root) {
+                nums[i] += nums[root - 1]*nums[i - root]; //left, right
             }
-            nums[i] = sum;
-        }
         return nums[n];
     }
 };
+
+//each number act as root
+//TLE
