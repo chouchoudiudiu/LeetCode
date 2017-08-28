ce7638dbc7da97ad5c194d2bb8eb5452fa5c5ab7
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-27 22:03:15 -0700

    Update 096.Unique_Binary_Search_Trees_2.cpp

--- src/096.Unique_Binary_Search_Trees_2.cpp
+++ src/096.Unique_Binary_Search_Trees_2.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-
-    int numTreeHelper(int n, vector<int>& nums)
-    {
+    int helper(int n, vector<int>& nums) {
         if(nums[n] != -1)
             return nums[n];
-            
-        int sum = 0;
-        for(int i = 1; i <= n; i++)
-            sum += numTreeHelper(i - 1, nums)*numTreeHelper(n - i, nums); 
-
-        nums[n] = sum;
-        return sum;
+        int cnt = 0;
+        for(int i = 1; i <= n; ++i) 
+            cnt += helper(i - 1, nums)*helper(n - i, nums);
+        nums[n] = cnt;
+        return cnt;
     }
-    int numTrees(int n) { //recursion
+    int numTrees(int n) {
         vector<int> nums(n + 1, -1);
         nums[0] = 1;
         nums[1] = 1;
-        
-        return numTreeHelper(n, nums);
+        return helper(n, nums);
     }
 };
+
+//each number act as root
+//TLE
