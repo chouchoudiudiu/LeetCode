fc68482ccdf820beebf1560c4f049ee0d6fd2bdd
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-29 11:52:00 -0700

    Update 495.Teemo_Attacking.cpp

--- src/495.Teemo_Attacking.cpp
+++ src/495.Teemo_Attacking.cpp
@@ -11,3 +11,20 @@ public:
         return total;
     }
 };
+
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        if(timeSeries.empty())
+            return 0;
+        long long total = duration; //last one
+        for(int i = 1; i < timeSeries.size(); ++i) {
+            if(timeSeries[i] - timeSeries[i - 1] < duration)
+                total += timeSeries[i] - timeSeries[i - 1];
+            else 
+                total += duration;
+        }
+        return total; 
+    }
+};
