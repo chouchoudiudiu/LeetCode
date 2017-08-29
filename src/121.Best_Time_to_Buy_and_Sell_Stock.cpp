aeee85af8fdf70b72fbb3a84043de0a4fe280fd7
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-29 10:54:44 -0700

    Update 121.Best_Time_to_Buy_and_Sell_Stock.cpp

--- src/121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ src/121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -11,3 +11,20 @@ public:
         return maxProfit;
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty())
+            return 0;
+        int low = prices[0], profit = 0;
+        for(int i = 1; i < prices.size(); ++i) {
+            if(prices[i] > low)
+                profit = max(profit, prices[i] - low);
+            if(prices[i] < low)
+                low = prices[i];
+        }
+        return profit;
+    }
+};
