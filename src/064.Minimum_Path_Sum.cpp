af65d3b02e1800849165d9ba7a8d320efec21f0a
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-29 15:49:39 -0700

    Update 064.Minimum_Path_Sum.cpp

--- src/064.Minimum_Path_Sum.cpp
+++ src/064.Minimum_Path_Sum.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
+        if(grid.empty())
+            return 0;
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> mat(m, vector<int>(n, 0));
+        mat[0][0] = grid[0][0];
+        for(int i = 1; i < m; ++i)
+            mat[i][0] = mat[i - 1][0] + grid[i][0];
+        for(int j = 1; j < n; ++j)
+            mat[0][j] = mat[0][j - 1] + grid[0][j];
+        for(int i = 1; i < m; ++i)
+            for(int j = 1; j < n; ++j) {
+                mat[i][j] = min(mat[i - 1][j], mat[i][j - 1]) + grid[i][j];
+            }
+        return mat[m - 1][n - 1];
+    }
+};
+///either way
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
         if (grid.empty())   
             return 0;
         int m = grid.size(), n = grid[0].size();
