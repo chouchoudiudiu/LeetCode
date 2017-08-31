adcbf0a5dba6b79d887319078689288961cb78d6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-08-31 14:01:51 -0700

    Update 611.Valid_Triangle_Number.cpp

--- src/611.Valid_Triangle_Number.cpp
+++ src/611.Valid_Triangle_Number.cpp
@@ -26,3 +26,28 @@ public:
 //try to fix a, fix b, move k; increment b, move k, 
 //move a, repeat
 //[2,2,3,4]
+
+//better solutions
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size(), cnt = 0;
+        if(n <= 2)
+            return 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 2; i < n; ++i) {
+            int j = 0, k = i - 1; 
+            while(j < k) {
+                if( nums[j] + nums[k] > nums[i]) {
+                    cnt += k - j;
+                    --k;
+                }
+                else
+                    ++j;
+            }
+        }
+        return cnt;
+    }
+};
+//fix好较大的两个
+//Assume a is the longest edge, b and c are shorter ones, to form a triangle, they need to satisfy len(b) + len(c) > len(a).
