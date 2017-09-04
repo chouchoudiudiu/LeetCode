f5d33eb25eef2cfd9a322d46988733eb56a4a785
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-04 14:47:17 -0700

    Update 397.Integer_Replacement.cpp

--- src/397.Integer_Replacement.cpp
+++ src/397.Integer_Replacement.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int integerReplacement(int n) {
-        long ln = n;
-        return helper(ln);
-    }
     int helper(long n) {
-        if (n == 1)
+        if(n == 1)
             return 0;
-        if (n % 2 == 0) //even
+        if(n%2 == 0)
             return helper(n/2) + 1;
-        else 
-            return min(helper(n - 1) + 1, helper(n + 1) + 1);
+        else
+            return min(helper(n - 1), helper(n + 1)) + 1;
+    }
+    int integerReplacement(int n) {
+        return helper(n); //long(n) is also fine
     }
 };
