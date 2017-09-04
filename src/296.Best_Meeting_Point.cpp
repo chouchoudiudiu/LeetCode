31025ccc2efc1e50790220291dbe86fb46d9e124
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-04 14:39:08 -0700

    Update 296.Best_Meeting_Point.cpp

--- src/296.Best_Meeting_Point.cpp
+++ src/296.Best_Meeting_Point.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
-//median in two direction
     int minTotalDistance(vector<vector<int>>& grid) {
-        if (grid.empty())
+        if(grid.empty())
             return 0;
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), total = 0;
         vector<int> x, y;
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j]) {
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j]) {
                     x.push_back(i);
                     y.push_back(j);
                 }
             }
+        int xlen = x.size(), ylen = y.size();
+        auto it_x = x.begin() + xlen/2, it_y = y.begin() + ylen/2;
+        nth_element(x.begin(), it_x, x.end());
+        nth_element(y.begin(), it_y, y.end());
+        int xmedian = *it_x, ymedian = *it_y;
+        for(int i = 0; i < m; ++i)
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j]) {
+                    total += abs(i - xmedian) + abs(j - ymedian);
+                }
+            }
+        return total;
+    }
+};
+//need to check proof
+//median in two direction
+/*
         sort(x.begin(), x.end());
         sort(y.begin(), y.end());
         //odd, how about even?
         int midX = x[x.size()/2], midY = y[y.size()/2];
-        int minDist = 0;
-        for (int i = 0; i < m; ++i)
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j]) {
-                    minDist += abs(i - midX) + abs(j - midY);
-                }
-            }
-        return minDist;
-    }
-};//need to check proof
+*/
