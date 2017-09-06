fec09937bf9e5f6d93db1b4d18458a03ddc102b1
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-06 15:18:35 -0700

    Update 551.Student_Attendance_Record_I.cpp

--- src/551.Student_Attendance_Record_I.cpp
+++ src/551.Student_Attendance_Record_I.cpp
@@ -15,3 +15,19 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int Acnt = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if(s[i] == 'A')
+                ++Acnt;
+            if(Acnt > 1)
+                return false;
+            if(i >= 2 && s[i] == 'L' && s[i - 1] == 'L' && s[i - 2] == 'L')
+                return false;
+        }
+        return true;
+    }
+};
