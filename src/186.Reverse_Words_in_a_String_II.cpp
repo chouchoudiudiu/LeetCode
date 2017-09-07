d598f923c79366ceb7e727921b6010428365fb00
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-06 22:07:19 -0700

    Update 186.Reverse_Words_in_a_String_II.cpp

--- src/186.Reverse_Words_in_a_String_II.cpp
+++ src/186.Reverse_Words_in_a_String_II.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     void reverseWords(string &s) {
+        int n = s.length();
+        reverse(s, 0, n - 1);
+        for(int i = 0, j = 0; i < n; ++i) {
+            if(s[i] == ' ') {
+                reverse(s, j, i - 1);
+                j = i + 1;
+            }
+            if(i == n - 1) 
+                reverse(s, j, i);
+        }
+    } 
+    void reverse(string& s, int l, int r) {
+        while(l < r)
+            swap(s[l++], s[r--]);
+    } 
+};
+
+class Solution {
+public:
+    void reverseWords(string &s) {
         reverse(s.begin(), s.end());
         for (int i = 0; i < s.length(); ++i) {
             if(s[i] != ' ') { //a start of a new word
