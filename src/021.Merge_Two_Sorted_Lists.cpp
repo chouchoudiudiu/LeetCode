7570f584bf1f16e0f7e2c2cec7ecf7b55d29a6c7
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-09 13:22:58 -0700

    Update 021.Merge_Two_Sorted_Lists.cpp

--- src/021.Merge_Two_Sorted_Lists.cpp
+++ src/021.Merge_Two_Sorted_Lists.cpp
@@ -10,20 +10,20 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode(0);
-        ListNode* head = dummy;
+        ListNode* node = dummy;
         while (l1 && l2) {
             if (l1->val < l2->val) {
-                dummy->next = l1;
+                node->next = l1;
                 l1 = l1->next;
             }
             else {
-                dummy->next = l2;
+                node->next = l2;
                 l2 = l2->next;
             }
-            dummy = dummy->next;
+            node = node->next;
         }
     
-        dummy->next = l1 ? l1 : l2;
-        return head->next;
+        node->next = l1 ? l1 : l2;
+        return dummy->next;
     }
 };
