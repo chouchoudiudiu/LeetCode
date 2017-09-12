42524d685d3b5bca032c4438b68c319ff5aca70c
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-12 11:10:56 -0700

    Update 535.Encode_and_Decode_TinyURL.cpp

--- src/535.Encode_and_Decode_TinyURL.cpp
+++ src/535.Encode_and_Decode_TinyURL.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
+
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         if(long2short.find(longUrl) != long2short.end())
             return long2short[longUrl];
-        long tmp = id;
-        string s = "";
-        while(tmp) {
+        string s;
+        long tmp = 0;
+        while(id) {
             s += dict[tmp%62];
-            tmp /= 62;
+            tmp /=  62;
         }
-        while(s.size() < 6)
-            s += '0';
-        long2short[longUrl] = s;
+        while(s.size() < 6) {
+            s = '0' + s;
+        }
+        long2short[longUrl] = s; 
         id2long[id] = longUrl;
-        ++id;
+        id++;
         return s;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        long tmp = 0;
+        long num = 0;
         for(int i = 0; i < shortUrl.size(); ++i) {
-            tmp += tmp*62 + dict.find(shortUrl[i]);
+            num = num*62 + dict.find(shortUrl[i]);
         }
-        return id2long.count(tmp) ? id2long[tmp] : "";
+        return id2long.count(num) ? id2long[num] :"";
     }
     
-    long id = 0;
     unordered_map<string, string> long2short;
-    unordered_map<long, string> id2long; //recover long id, recvoer long url
+    unordered_map<long, string> id2long;
+    //recover long id, recvoer long url
     string dict = "0123456789abcedfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    long id = 0;
 };
 
 // Your Solution object will be instantiated and called as such:
