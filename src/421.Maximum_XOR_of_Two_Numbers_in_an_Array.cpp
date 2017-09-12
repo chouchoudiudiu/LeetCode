d8aa5e7fa48b11764de34dcb1ae5b4266aee30ff
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-12 15:50:17 -0700

    Update 421.Maximum_XOR_of_Two_Numbers_in_an_Array.cpp

--- src/421.Maximum_XOR_of_Two_Numbers_in_an_Array.cpp
+++ src/421.Maximum_XOR_of_Two_Numbers_in_an_Array.cpp
@@ -6,7 +6,7 @@ public:
             mask = mask | (1<<i); //mask作为前缀比如 110000, 100000
             unordered_set<int> set; 
             for (auto v : nums) 
-                set.insert(mask & v); //0 is always in set
+                set.insert(mask & v); //0 is always in set as long as there is a number that is 0 in such bit
             int tmp = max | (1<<i); //仅该位为1，加上之前的max(之前已经为1的)
             for (auto pre : set) {
                 if (set.find(tmp ^ pre) != set.end()) { //11000 tmp 遇到00100 mask&v
