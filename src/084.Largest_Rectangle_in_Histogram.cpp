617b48ccf602254a695e86e3b873e9da6d885a03
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-12 16:17:49 -0700

    Update 084.Largest_Rectangle_in_Histogram.cpp

--- src/084.Largest_Rectangle_in_Histogram.cpp
+++ src/084.Largest_Rectangle_in_Histogram.cpp
@@ -3,10 +3,9 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> stk;
         stk.push(-1);
-        int n = heights.size();
-        int maxArea = 0;
+        int n = heights.size(), res = 0;
         vector<int> width(n, 0);
-        for (int i = 0; i < n; ++i) {
+        for(int i = 0; i < n; ++i) {
             while(!stk.empty() && stk.top() >= 0 && heights[i] <= heights[stk.top()])
                 stk.pop();
             width[i] = i - stk.top() - 1;
@@ -14,19 +13,14 @@ public:
         }
         stk = stack<int>();
         stk.push(n);
-        for (int i = n - 1; i >= 0; --i) {
+        for(int i = n - 1; i >= 0; --i) {
             while(!stk.empty() && stk.top() <= n - 1 && heights[i] <= heights[stk.top()])
                 stk.pop();
             width[i] += stk.top() - i;
             stk.push(i);
         }
-        
-        for (int i = 0; i < n; ++i) {
-            maxArea = max(maxArea, heights[i]*width[i]);
-        }
-        
-        return maxArea;
+        for(int i = 0; i < n; ++i)
+            res = max(res, heights[i]*width[i]);
+        return res;
     }
 };
-
-//seen shorter version
