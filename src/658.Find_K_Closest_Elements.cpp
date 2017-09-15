2d4c0bfb9414c05a4f3731c4b8c2ff46914bac38
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-15 12:26:31 -0700

    Update 658.Find_K_Closest_Elements.cpp

--- src/658.Find_K_Closest_Elements.cpp
+++ src/658.Find_K_Closest_Elements.cpp
@@ -15,3 +15,24 @@ public:
         return res;
     }
 };
+//////////
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int idx = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int i = idx - 1, j = idx;
+        while(k--) {
+            if(i < 0 || (j < arr.size() && abs(arr[i] - x) > abs(arr[j] - x)))
+                ++j;
+            else
+                --i;
+        }
+        return vector<int>(arr.begin() + i + 1, arr.begin() + j);
+    }
+};
+
+/*
+The idea is to find the first number which is equal to or greater than x in arr. Then, we determine the indices of the start and the end of a subarray in arr, where the subarray is our result. The time complexity is O(logn + k).
+
+In the following code, arr[index] is the first number which is euqal to or geater than x (if all numbers are less than x, index is arr.size()), and the result is arr[i+1, i+2, ... j].
+*/
