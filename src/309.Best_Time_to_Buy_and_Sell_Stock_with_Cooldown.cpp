b7bdd1bd44ba52f858e5fd4ba4259391a63e32d1
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-16 12:48:17 -0700

    Update 309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp

--- src/309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
+++ src/309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> dp(n, 0);
+        for(int i = 1; i < n; ++i) {
+            dp[i] = dp[i - 1];
+            for(int j = 0; j < i; ++j)
+                dp[i] = max(dp[i], prices[i] - prices[j] + (j - 2 >= 0 ? dp[j - 2] : 0));
+        }
+        return prices.empty() ? 0 : dp.back();
+    }
+};
+/*
+ * Let f[i] be largest profit when given the stock price on day 0, 1, ..., i.
+ * Consider the possible actions on day i, we have the following two choices.
+ *
+ * 1) Cooldown on at day, then f[i] = f[i - 1];
+ * 2) Sell the stock, then we need to determine the best day to buy in.
+ *    If we buy in on day j (j < i), then we must cooldown on day j - 1,
+ *    and then the optimal structure property applies to day 0, 1, ..., j - 2.
+ *    Therefore, f[i] = max(f[j - 2] + price[i] - price[j]), 0 <= j < i.
+ * Note that buying in on day i is meaningless since we can never sell it out under the constrain [0, i].
+ *
+ * For the boundary case, define f[i] = 0 for all i <= 1.
+ *
+ * The final answer is f[n - 1], and the entire DP solution runs in O(n^2) time.
+ * Question: How to improve the runtime to O(n)?
+ */
+//////////////////////////////////
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         if (prices.empty())
 	        return 0;
 
