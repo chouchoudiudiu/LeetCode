51271179cb2b68dcf88d3e630b38d97dbed7ea29
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-16 00:45:12 -0700

    Update 651.4_Keys_Keyboard.cpp

--- src/651.4_Keys_Keyboard.cpp
+++ src/651.4_Keys_Keyboard.cpp
@@ -15,3 +15,15 @@ A, A, A, Ctrl A, Ctrl C, Ctrl V, Ctrl V
 Here we have n = 7 and we used i = 3 steps to reach AAA
 Then we use the remaining n - i = 4 steps: Ctrl A, Ctrl C, Ctrl V, Ctrl V, to reach n - i - 1 = 3 copies of AAA
 */
+class Solution {
+public:
+    int maxA(int N) {
+        vector<int> dp(N + 1);
+        for(int i = 0; i <= N; ++i) {
+            dp[i] = i;
+            for(int j = 1; j <= i - 3; ++j)
+                dp[i] = max(dp[i], dp[j]*(i - j - 1));
+        }
+        return dp[N];
+    }
+};
