1306f6e3c3e58bbd4043c8e7f462938442ea7421
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-17 18:48:09 -0700

    Update 010.Regular_Expression_Matching.cpp

--- src/010.Regular_Expression_Matching.cpp
+++ src/010.Regular_Expression_Matching.cpp
@@ -7,7 +7,7 @@ public:
             return (*p == *s || *p == '.' && *s != '\0') && isMatch(s + 1, p + 1); 
         }
         while (*p == *s || *p == '.' && *s != '\0') {
-            if (isMatch(s, p + 2)) //调用递归函数匹配s和去掉前两个字符的p;因为p中a*可以有0或任意个a，可以当做0个
+            if (isMatch(s, p + 2)) //调用递归函数匹配s和去掉前两个字符的p;因为p中a*可以有0或任意个a，可以当做0个；ttea and t*ea
                 return true;
             ++s;
         }
