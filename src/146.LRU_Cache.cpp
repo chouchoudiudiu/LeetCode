803184b1e816d6b8eefda14082b46f8a5d6d9b26
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-21 09:06:04 -0700

    Update 146.LRU_Cache.cpp

--- src/146.LRU_Cache.cpp
+++ src/146.LRU_Cache.cpp
@@ -1,5 +1,47 @@
 class LRUCache {
 public:
+    LRUCache(size_t capacity) {
+        m_capacity = capacity;
+    }
+    
+    int get(int key) {
+        auto found_iter = m_map.find(key);
+        if(found_iter == m_map.end()) //key doesn't exist
+            return -1;
+        m_cache.splice(m_cache.begin(), m_cache, found_iter->second);// void splice (iterator position, list& x, iterator i);
+        return found_iter->second->second;//直接改变it->second的具体的值，例如重新指向cache开头 //move the node corresponding to key to front
+    } //return value of the node
+    
+    void put(int key, int value) {
+        auto found_iter = m_map.find(key);
+        if(found_iter != m_map.end()) {
+            m_cache.splice(m_cache.begin(), m_cache, found_iter->second);//move the node corresponding to key to front
+            found_iter->second->second = value;//update value of the node
+            return;
+        }
+        if(m_map.size() == m_capacity) {//reached capacity
+            int key_to_del = m_cache.back().first;
+            m_cache.pop_back(); //remove node in list;
+            m_map.erase(key_to_del); //remove key in map
+        }
+        m_cache.emplace_front(key, value);//create new node in list
+        m_map[key] = m_cache.begin();//create correspondence between key and node
+    }
+    
+    size_t m_capacity;
+    list<pair<int, int>> m_cache; //m_cache_iter->first: key, m_cache_iter->second: value;
+    unordered_map<int, list<pair<int, int>>::iterator> m_map; //m_map_iter->first: key, m_map_iter->second: list iterator;
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+
+class LRUCache {
+public:
     LRUCache(int capacity) {
         cap = capacity;
     }
