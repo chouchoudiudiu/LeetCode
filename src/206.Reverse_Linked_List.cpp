543676643a591ab0a7641a39e6c87dc84dfa8190
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-28 09:22:58 -0700

    Update 206.Reverse_Linked_List.cpp

--- src/206.Reverse_Linked_List.cpp
+++ src/206.Reverse_Linked_List.cpp
@@ -37,7 +37,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if (!head || !head->next)
-            return head;
+            return head;                 //return head!
         ListNode* prev = NULL;
         ListNode* curr = head;
         while (curr) {
