d3f40802c604df870723b7e96fe9fdfe839a15b0
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-28 10:00:04 -0700

    Update 398.Random_Pick_Index.cpp

--- src/398.Random_Pick_Index.cpp
+++ src/398.Random_Pick_Index.cpp
@@ -1,31 +1,23 @@
+ //reservior sampling
 class Solution {
 public:
- //reservior sampling
-    Solution(vector<int> nums) {
-        this->nums = nums;
+    Solution(vector<int> nums): v(nums) {
+        
     }
     
     int pick(int target) {
-        int n = nums.size(), cnt = 0, first = 0, res = 0; //must exist
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == target) {
-                ++cnt;
-                res = first = i;
-                break;
-            }
-        }
-        
-        for (int i = first + 1; i < n; ++i) {
-            if (nums[i] == target) {
-                ++cnt;
-                int j = rand()%cnt;
-                if (j == 0)
-                    res = i;
-            }
+        int res = -1, cnt = 0;
+        for(int i = 0; i < v.size(); ++i) {
+            if(v[i] != target)
+                continue;
+            ++cnt;
+            if(rand()%cnt == 0)
+                res = i;
         }
         return res;
     }
-    vector<int> nums;
+    
+    vector<int> v;
 };
 
 /**
@@ -33,3 +25,8 @@ public:
  * Solution obj = new Solution(nums);
  * int param_1 = obj.pick(target);
  */
+/*
+我们定义两个变量，计数器cnt和返回结果res，我们遍历整个数组，如果数组的值不等于target，直接跳过；
+如果等于target，计数器加1，然后我们在[0,cnt)范围内随机生成一个数字，如果这个数字是0，我们将res赋值为i即可
+*/
+
