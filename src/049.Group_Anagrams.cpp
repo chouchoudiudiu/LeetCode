f0c536bd1c61ebbb197a324289b9baea75f65d2c
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-09-29 10:12:39 -0700

    Update 049.Group_Anagrams.cpp

--- src/049.Group_Anagrams.cpp
+++ src/049.Group_Anagrams.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, vector<int>>  m;
-        for (int i = 0; i < strs.size(); ++i) {
-            string s = strs[i]; //dont modify the value of strs
-            sort(s.begin(), s.end());
-            //if (m.count(s))
-                m[s].push_back(i);
-            //else
-             //   m.insert({s, {i}}); // {i} as a new vector
+        unordered_map<string, vector<string>> m;
+        for(auto v : strs) {
+            string s = v; //dont modify the value of strs
+            sort(s.begin(), s.end()); 
+            m[s].push_back(v);
         }
-        for (auto v:m) {
-            auto vec = v.second;
-            vector<string> temp;
-            for (int i = 0; i < vec.size(); ++i) {
-                temp.push_back(strs[vec[i]]);
-            }
-            res.push_back(temp);
-        }
+        for(auto v : m) 
+            res.push_back(v.second);
         return res;
     }
 };
