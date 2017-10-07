b39c20c6bef1c67ee36790f8f334657018039922
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-07 05:31:03 -0700

    Update 6xxx_BST2DLL.cpp

--- src/6xxx_BST2DLL.cpp
+++ src/6xxx_BST2DLL.cpp
@@ -7,7 +7,7 @@ struct TreeNode{
     TreeNode* right;
     TreeNode(int value) : val(value), left(NULL), right(NULL) {}
 };
-
+//usse original left and right of treenode
 void BST2DLL(TreeNode* p, TreeNode*& prev, TreeNode*& head) {
     if(!p)
         return;
@@ -66,3 +66,63 @@ int main()
 // This is a modified in-order traversal adapted to this problem.
 // prev (init to NULL) is used to keep track of previously traversed node.
 // head pointer is updated with the list's head as recursion ends.
+
+
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ * Definition of Doubly-ListNode
+ * class DoublyListNode {
+ * public:
+ *     int val;
+ *     DoublyListNode *next, *prev;
+ *     DoublyListNode(int val) {
+ *         this->val = val;
+           this->prev = this->next = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of tree
+     * @return: the head of doubly list node
+     */
+    DoublyListNode* bstToDoublyList(TreeNode* root) {
+        DoublyListNode * head = NULL; 
+        if(!root)
+            return head;
+        DoublyListNode *last_node = NULL;
+        convert( root, last_node);
+        while( last_node && last_node->prev){
+            last_node = last_node -> prev;
+        }
+        return last_node;
+    }
+    
+    void convert(TreeNode * root, DoublyListNode*& pLastNode){
+        if(!root){
+            return;
+        }
+        if( root ->left)
+            convert(root->left, pLastNode);
+        
+        DoublyListNode * current = new DoublyListNode(root->val);
+        current -> prev = pLastNode;
+        if (pLastNode){
+            pLastNode->next = current;
+        }
+        pLastNode = current;
+        if(root ->right)
+            convert(root->right, pLastNode);
+    }
+};
