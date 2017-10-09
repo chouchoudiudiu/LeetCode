fcdcd4a8b27fc386be37d01182b3ecc89a5d0a0b
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 11:52:38 -0700

    Update 7***_sortArraySquares.cpp

--- src/7***_sortArraySquares.cpp
+++ src/7***_sortArraySquares.cpp
@@ -1,4 +1,19 @@
 void sortSquares(vector<int>& nums, vector<int>& sorted) {
+    size_t l = 0, r = nums.size() - 1, idx = r;
+    for(size_t i = 0; i < nums.size(); ++i) {
+        if(abs(nums[l]) < abs(nums[r])) {
+            sorted[idx--] = nums[r]*nums[r];
+            --r;
+        }
+        else {
+            sorted[idx--] = nums[l]*nums[l];
+            ++l;
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+void sortSquares(vector<int>& nums, vector<int>& sorted) {
     size_t i = 0, j = nums.size() - 1;
     while(i < j) {
         if(nums[i]*nums[i] < nums[j]*nums[j]) {
