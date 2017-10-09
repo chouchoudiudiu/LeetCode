00ac8f6940be05870f027e2ca47c87f2712e3ab3
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 15:14:49 -0700

    Update 349.Intersection_of_Two_Arrays.cpp

--- src/349.Intersection_of_Two_Arrays.cpp
+++ src/349.Intersection_of_Two_Arrays.cpp
@@ -12,3 +12,26 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> res;
+        int i = 0, j = 0;
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        while(i < nums1.size() && j < nums2.size()) {
+            if(nums1[i] == nums2[j]) {
+                res.insert(nums1[i]);
+                ++i;
+                ++j;
+            }
+            else if(nums1[i] < nums2[j])
+                ++i;
+            else
+                ++j;
+        }
+        return vector<int>(res.begin(), res.end());
+    }
+};
