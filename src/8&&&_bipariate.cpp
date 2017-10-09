56d13917139982151101dbc5b3621a80106d1c66
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 16:16:46 -0700

    Create 8&&&_bipariate.cpp

---
+++ src/8&&&_bipariate.cpp
@@ -0,0 +1,6 @@
+/*
+validate bipariate graph
+Input friend's relation {{1, 2}, {2, 3}, {3, 4}}, could you split all the users into two groups and for the user in each group, 
+all the users do not know each other. So the expected output should be group1 {1, 3}, group2 {2, 4}. 
+If it can't be splitted into two groups, just return "impossible".
+*/
