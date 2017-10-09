fc6f683343fec0ebff67b5e744ad9b25bcc0f534
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 16:58:26 -0700

    Update 8)))_removeInvalidParen.cpp

--- src/8)))_removeInvalidParen.cpp
+++ src/8)))_removeInvalidParen.cpp
@@ -1,3 +1,44 @@
+https://instant.1point3acres.com/thread/180240
+https://instant.1point3acres.com/thread/161681
+用一个counter，每次遇到左括号加一；每次遇到右括号先判断counter是否为零：
+为零说明右括号多了，删除当前右括号，不为零说明合法，counter减一。
+遍历一遍以后可以看看counter是否大于零，如果是则从右数起删除多余的左括号。
+
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> strs;
+        int cnt = 0;
+        string output, res;
+        for(auto c : s) {
+            if(c != '(' && c != ')')
+                continue;
+            else if(c == '(') {
+                ++cnt;
+                output += c;
+            }
+            else {
+                if(cnt == 0)
+                    continue; //not adding
+                else {
+                    output += c;
+                    --cnt;
+                }
+            }
+        }
+        for(int i = output.size() - 1; i >= 0; --i) {
+            if(cnt > 0 && output[i] == '(')
+                --cnt;
+            else
+                res += output[i];
+        }
+        reverse(res.begin(), res.end());
+        strs.push_back(res);
+        return strs;
+    }
+};
+
+
 //this solution is actually incorrect...........
 class Solution {
 public:
