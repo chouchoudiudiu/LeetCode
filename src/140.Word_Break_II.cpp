0b15c2a463535ff1bff013953985cc0c29460a25
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-10 03:35:56 -0700

    Update 140.Word_Break_II.cpp

--- src/140.Word_Break_II.cpp
+++ src/140.Word_Break_II.cpp
@@ -29,3 +29,34 @@ public:
 
 //这里加上一个possible数组，如同WordBreak I里面的DP数组一样，用于记录区间拆分的可能性
 //Possible[i] = true 意味着 [i,n]这个区间上有解*/
+class Solution {
+public:
+    void helper(string s, unordered_set<string>& dict, string out, int idx, vector<string>& res, vector<bool>& dp) {
+        if(idx == s.length()) {
+            out.pop_back();
+            res.push_back(out);
+        }
+        for(int i = idx; i < s.length(); ++i) {
+            string tmp = s.substr(idx, i - idx + 1);
+            if(dict.find(tmp) != dict.end() && dp[i + 1]) {
+                int size = res.size();
+                helper(s, dict, out + tmp + " ", i + 1, res, dp);
+                if(res.size() == 1)
+                    return;
+                if(res.size() == size)
+                    dp[i + 1] = false;
+            }
+        }
+    }
+
+        vector<string> wordBreak(string s, vector<string>& wordDict) {
+            if(s.empty() || wordDict.empty())
+                return {};
+            vector<string> res;
+            vector<bool> dp(s.length(), true);
+            unordered_set<string> dict(wordDict.begin(), wordDict.end());
+            string out;
+            helper(s, dict, out, 0, res, dp);
+            return res;
+        }
+};
