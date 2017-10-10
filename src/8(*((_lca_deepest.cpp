9d20813742fbdcb4052bf0ccaa4feefa69ffc195
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 17:28:33 -0700

    Update 8(*((_lca_deepest.cpp

--- src/8(*((_lca_deepest.cpp
+++ src/8(*((_lca_deepest.cpp
@@ -36,3 +36,61 @@ int commonAncestor(TreeNode * root){
     cout << "Tree depth:" << depth << endl;
     return LCA->val;
 }
+
+
+ #include<iostream>
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+
+
+TreeNode* dfs(TreeNode* root, int & depth){
+    if(!root){
+        depth = 0;
+        return NULL;
+    }
+    
+    int ldepth, rdepth;
+    TreeNode * left = dfs(root->left, ldepth);
+    TreeNode * right = dfs(root->right, rdepth);
+    depth = 1 + max(ldepth, rdepth);
+    
+    if(ldepth == rdepth) return root;
+    else if(ldepth < rdepth) return right;
+    else return left;
+}
+
+int commonAncestor(TreeNode * root){
+    int depth;
+    TreeNode* LCA = dfs(root, depth);
+    cout << "Tree depth:" << depth << endl;
+    return LCA->val;
+}
+
+int main() {
+    TreeNode* A = new TreeNode(1);
+    TreeNode* B = new TreeNode(2);
+    TreeNode* C = new TreeNode(3);
+    TreeNode* D = new TreeNode(4);
+    TreeNode* E = new TreeNode(5);
+    TreeNode* F = new TreeNode(6);
+    TreeNode* H = new TreeNode(11);
+    TreeNode* I = new TreeNode(17);
+    A->left = B;
+    A->right = C;
+    C->left = E;
+    C->right = F;
+    B->left = D;
+    F->right = H;
+    
+    E->left = I;
+    cout<<commonAncestor(A)<<endl;;
+    
+    return 0;
+    
+}
