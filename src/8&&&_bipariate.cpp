e376e6a8c24b63c66e0f1200ddd76efce3fad699
chouchoudiudiu <chouchoudiudiu@gmail.com>
2017-10-09 17:02:32 -0700

    Update 8&&&_bipariate.cpp

--- src/8&&&_bipariate.cpp
+++ src/8&&&_bipariate.cpp
@@ -4,3 +4,4 @@ Input friend's relation {{1, 2}, {2, 3}, {3, 4}}, could you split all the users 
 all the users do not know each other. So the expected output should be group1 {1, 3}, group2 {2, 4}. 
 If it can't be splitted into two groups, just return "impossible".
 */
+http://www.geeksforgeeks.org/bipartite-graph/
