1b6357ebbac6b23254d0e3c711484066d8957011
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-05-07 16:47:36 -0700

    Update 560.Subarray_Sum_Equals_K.cpp

--- src/560.Subarray_Sum_Equals_K.cpp
+++ src/560.Subarray_Sum_Equals_K.cpp
@@ -12,3 +12,20 @@ public:
         return cnt;
     }
 };
+
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> un_map; //prefix sum, count
+        int cnt = 0, sum = 0;
+        un_map[0] = 1;
+        for(size_t i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+            if(un_map.count(sum - k))
+                cnt += un_map[sum - k];
+            un_map[sum]++;
+        }
+        return cnt;
+    }
+};
