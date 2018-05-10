24aad7e18a8e088ca8a7c313c2c4c82ed64c39c4
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-05-10 13:16:30 -0700

    Update 279.Perfect_Squares.cpp

--- src/279.Perfect_Squares.cpp
+++ src/279.Perfect_Squares.cpp
@@ -41,3 +41,20 @@ public:
         return steps;
     }
 };
+
+
+///
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= n; ++i) {
+            dp[i] = i;
+            for(int j = 1; j <= sqrt(i); ++j) {
+                dp[i] = min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+        return dp[n];
+    }
+};
