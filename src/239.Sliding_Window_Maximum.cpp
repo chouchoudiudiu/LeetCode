b78bf5a202d3bb35e3ab2a51d4153b0d9259da5b
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-06-28 16:00:17 -0700

    Update 239.Sliding_Window_Maximum.cpp

--- src/239.Sliding_Window_Maximum.cpp
+++ src/239.Sliding_Window_Maximum.cpp
@@ -11,13 +11,13 @@ public:
                 dq.pop_back();
             dq.push_back(i);
         }
-        for (int i = k; i < nums.size(); ++i) {
-            res.push_back(nums[dq.front()]); //always keeps the maximum in the first
-            while (!dq.empty() && nums[i] >= nums[dq.back()])
+        for(int i = k; i < nums.size(); ++i) {
+            res.push_back(nums[dq.front()]);
+            while(!dq.empty() && nums[dq.back()] <= nums[i])
                 dq.pop_back();
-            if (!dq.empty() && i - dq.front() >= k) //may or may not in good window, if not, retire
-                dq.pop_front();
             dq.push_back(i);
+            if(i - dq.front() >= k) //may or may not in good window, if not, retire
+                dq.pop_front();
         }
         res.push_back(nums[dq.front()]);
         return res;
