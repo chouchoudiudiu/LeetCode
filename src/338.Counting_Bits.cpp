33790c8389906135318d8f68e63da98f9b69e247
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-08-30 14:52:45 -0700

    Update 338.Counting_Bits.cpp

--- src/338.Counting_Bits.cpp
+++ src/338.Counting_Bits.cpp
@@ -28,3 +28,12 @@ public:
         return res;
     }
 };
+//
+ vector<int> countBits(int num) {
+        vector<int> res(num + 1, 0);
+        res[1] = 1;
+        for(int i = 2; i <= num; ++i) {
+            res[i] = res[i>>1] + i%2
+        }
+        return res;
+    }
