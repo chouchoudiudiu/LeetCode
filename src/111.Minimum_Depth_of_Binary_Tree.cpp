0c46586c9f978072620c5555c185ab0ce31fd695
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-09-26 11:21:45 -0700

    Update 111.Minimum_Depth_of_Binary_Tree.cpp

--- src/111.Minimum_Depth_of_Binary_Tree.cpp
+++ src/111.Minimum_Depth_of_Binary_Tree.cpp
@@ -20,3 +20,22 @@ public:
         return 1 + min(minDepth(root->left), minDepth(root->right));
     }
 };
+
+=======
+    
+   class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        if(!root->left && !root->right)
+            return 1;
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+        if(!root->left)
+            return right + 1;
+        if(!root->right)
+            return left + 1;
+        return min(left, right) + 1;
+    }
+};  
