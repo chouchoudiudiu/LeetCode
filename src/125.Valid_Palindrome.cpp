3f112801bd4d5ce6d5bae80c7c955f70e1ada56a
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-02 11:54:52 -0700

    Update 125.Valid_Palindrome.cpp

--- src/125.Valid_Palindrome.cpp
+++ src/125.Valid_Palindrome.cpp
@@ -16,3 +16,23 @@ public:
         return true;
     }
 };
+
+======
+    class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int l = 0, r = n - 1;
+        while(l < r) {
+            while(l < r && !isalnum(s[l]))
+                ++l;
+            while(l < r && !isalnum(s[r]))
+                --r;
+            if(l > r)
+                return true;
+            if(tolower(s[l++]) != tolower(s[r--]))
+                return false;
+        }
+        return true;
+    }
+};
