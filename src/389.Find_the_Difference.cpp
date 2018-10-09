771cc330795d348aadfa8b2891a8af06e079d790
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-09 13:35:14 -0700

    Update 389.Find_the_Difference.cpp

--- src/389.Find_the_Difference.cpp
+++ src/389.Find_the_Difference.cpp
@@ -24,3 +24,16 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int freq[26] = {0};
+        for(auto c : s) 
+            ++freq[c - 'a'];
+        for(auto c : t) {
+            if(--freq[c - 'a'] < 0)
+                return c;
+        }
+    }
+};
