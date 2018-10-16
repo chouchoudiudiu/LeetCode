2f47d11c8e3588f2d7d940c6fd0eefd1f5355146
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-16 13:50:52 -0700

    Update 415.Add_Strings.cpp

--- src/415.Add_Strings.cpp
+++ src/415.Add_Strings.cpp
@@ -19,3 +19,25 @@ public:
         return res;
     }
 };
+
+=======
+    
+    class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int m = num1.length(), n = num2.length();
+        string s(max(m, n) + 1, '0');
+        int sum = 0, carry = 0, i = m - 1, j = n - 1, t = 0;
+        while(i >= 0 || j >= 0 || carry > 0) {
+            sum = carry + (i >= 0 ? num1[i] - '0' : 0) + (j >= 0 ? num2[j] - '0' : 0);
+            s[t++] = sum%10 + '0';
+            carry = sum/10;
+            --i;
+            --j; 
+        }
+        reverse(s.begin(), s.end());
+        if(s[0] == '0')
+            s.erase(s.begin());
+        return s;
+    }
+};
