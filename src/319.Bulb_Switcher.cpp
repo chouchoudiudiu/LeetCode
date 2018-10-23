503fe67463ed67cf6da42dd8bb020666edb978a6
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-22 22:14:03 -0700

    Update 319.Bulb_Switcher.cpp

--- src/319.Bulb_Switcher.cpp
+++ src/319.Bulb_Switcher.cpp
@@ -4,3 +4,9 @@ public:
         return sqrt(n);
     }
 };
+
+We want to find how many bulbs are on after n rounds (In the end).
+That means we need to find out how many perfect square numbers are NO MORE than n.
+The number of perfect square numbers which are no more than n, is the square root of the 
+maximum perfect square number which is NO MORE than n
+
