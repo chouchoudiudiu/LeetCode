85269f1b13b261a71d60cd3a658be8d83ebc505e
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-24 15:20:50 -0700

    Update 120.Triangle.cpp

--- src/120.Triangle.cpp
+++ src/120.Triangle.cpp
@@ -16,3 +16,18 @@ public:
         return m[0];
     }
 };
+
+//////
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty())
+            return 0;
+        int n = triangle.size();
+        vector<int> m = triangle.back(); //last line
+        for(int i = n - 2; i >= 0; --i) 
+            for(int j = 0; j <= i; ++j)  //算下个j的时候，不需要用到旧的j，一直是往大的走的，所以不需要一个tmp
+                m[j] = min(m[j], m[j + 1]) + triangle[i][j];   
+        return m[0];
+    }
+};
