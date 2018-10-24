86c412e33e113725955c2dec83bc16c3e3be67ac
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-23 22:04:53 -0700

    Update 063.Unique_Paths_II.cpp

--- src/063.Unique_Paths_II.cpp
+++ src/063.Unique_Paths_II.cpp
@@ -21,3 +21,26 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+=======
+    class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid.empty())
+            return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        int dp[m][n];
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for(size_t i = 1; i < m; ++i) {
+            dp[i][0] = obstacleGrid[i][0] == 0 ? dp[i - 1][0] : 0;
+        }
+        for(size_t j = 1; j < n; ++j) {
+            dp[0][j] = obstacleGrid[0][j] == 0 ? dp[0][j - 1] : 0;
+        }
+        for(size_t i = 1; i < m; ++i)
+            for(size_t j = 1; j < n; ++j)
+                dp[i][j] = obstacleGrid[i][j] == 0 ? dp[i - 1][j] + 
+                dp[i][j - 1] : 0;
+        return dp[m - 1][n - 1];
+    }
+};
