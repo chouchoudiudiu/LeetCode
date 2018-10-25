2bdec1113d1a099965600605c69cd95488e5650c
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-10-24 17:46:19 -0700

    Update 434.Number_of_Segments_in_a_String.cpp

--- src/434.Number_of_Segments_in_a_String.cpp
+++ src/434.Number_of_Segments_in_a_String.cpp
@@ -12,3 +12,18 @@ public:
         return cnt;
     }
 };
+
+====
+    class Solution {
+public:
+    int countSegments(string s) {
+        int cnt = 0, n = s.length();
+        for(int i = 0; i < n; ++i) {
+            if(s[i] == ' ' && i > 0 && s[i - 1] != ' ')
+                ++cnt;
+            if(i == n - 1 && s[i] != ' ')
+                ++cnt;
+        }
+        return cnt;
+    }
+};
