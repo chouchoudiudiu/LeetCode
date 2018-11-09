1d110a15ffd96bb938b0d471fd148b3aff8bae18
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-11-08 16:55:55 -0800

    Update 445.Add_Two_Numbers_II.cpp

--- src/445.Add_Two_Numbers_II.cpp
+++ src/445.Add_Two_Numbers_II.cpp
@@ -43,3 +43,41 @@ public:
         return head;
     }
 };
+
+/// 一开始那里。。。没有必要
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        while(l1) {
+           s1.push(l1->val);
+           l1 = l1->next; 
+        }
+        while(l2) {
+           s2.push(l2->val);
+           l2 = l2->next; 
+        }
+        int carry = 0;
+        ListNode* head = NULL;
+        while(!s1.empty() || !s2.empty() || carry > 0) {
+            int sum = carry;
+            if(!s1.empty()) {
+                sum += s1.top();
+                s1.pop();
+            }
+            if(!s2.empty()) {
+                sum += s2.top();
+                s2.pop();
+            }
+            ListNode* node = new ListNode(sum%10);
+            carry = sum/10;
+            if(!head)
+                head = node;
+            else {
+                node->next = head;
+                head = node;
+            }
+        }
+        return head;
+    }
+};
