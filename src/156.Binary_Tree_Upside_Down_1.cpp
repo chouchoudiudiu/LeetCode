30aa703ed3df2cdc576ae50a6eb97639744d9967
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-11-24 10:59:23 -0800

    Update 156.Binary_Tree_Upside_Down_1.cpp

--- src/156.Binary_Tree_Upside_Down_1.cpp
+++ src/156.Binary_Tree_Upside_Down_1.cpp
@@ -12,7 +12,7 @@ public:
     TreeNode* upsideDownBinaryTree(TreeNode* root) {
         if (!root || !root->left)
             return root;
-        TreeNode* l = root->left, *r = root->right;
+        TreeNode* l = root->left;//, *r = root->right;
         TreeNode* res = upsideDownBinaryTree(l);
         l->left = r;
         l->right = root;
