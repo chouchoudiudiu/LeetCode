8bef4db1ebda7696acaab497ac7871eef10ea76e
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-12-02 22:45:14 -0800

    Update 253.Meeting_Rooms_II.cpp

--- src/253.Meeting_Rooms_II.cpp
+++ src/253.Meeting_Rooms_II.cpp
@@ -37,7 +37,7 @@ public:
     int minMeetingRooms(vector<Interval>& intervals) {
         sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) {
             return a.start < b.start;
-        });
+        }); //也是必须的
         priority_queue<int, vector<int>, greater<int>> pq;
         for(auto v : intervals) {
             if(!pq.empty() && v.start >= pq.top())
@@ -52,4 +52,6 @@ public:
 且首元素小于等于当前区间的起始时间，我们去掉堆中的首元素，把当前区间的结束时间压入堆，
 由于最小堆是小的在前面，那么假如首元素小于等于起始时间，说明上一个会议已经结束，
 可以用该会议室开始下一个会议了，所以不用分配新的会议室，遍历完成后堆中元素的个数即为需要的会议室的个数
+
+按开始时间排序是必须的，这样才能说前面的一个会议已经结束了
 */
