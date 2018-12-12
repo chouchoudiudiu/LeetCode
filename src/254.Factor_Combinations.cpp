9c7d3f7d345ea9955567a9928624d6cae27fd323
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-12-11 20:06:44 -0800

    Update 254.Factor_Combinations.cpp

--- src/254.Factor_Combinations.cpp
+++ src/254.Factor_Combinations.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     vector<vector<int>> getFactors(int n) {
         vector<vector<int>> res;
-        helper(n, 2, {}, res);
+        vector<int> out;
+        helper(n, 2, out, res);
         return res;
     }
-    void helper(int n, int start, vector<int> out, vector<vector<int>> &res) {
+    void helper(int n, int start, vector<int>& out, vector<vector<int>> &res) {
         if (n == 1) {
-            if (out.size() > 1) res.push_back(out);
+            if (out.size() > 1) //important, otherwise may contain self
+                res.push_back(out);
         } else {
-            for (int i = start; i <= n; ++i) {
+            for (int i = start; i <= n; ++i) { //i <= n
                 if (n % i == 0) {
                     out.push_back(i);
                     helper(n / i, i, out, res);
