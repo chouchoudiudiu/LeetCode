2491de752131ad3eb1c0fc3dc3e5f569f5dba95b
chouchoudiudiu <chouchoudiudiu@gmail.com>
2018-12-11 20:06:56 -0800

    Update 254.Factor_Combinations.cpp

--- src/254.Factor_Combinations.cpp
+++ src/254.Factor_Combinations.cpp
@@ -10,7 +10,8 @@ public:
         if (n == 1) {
             if (out.size() > 1) //important, otherwise may contain self
                 res.push_back(out);
-        } else {
+        } 
+        else {
             for (int i = start; i <= n; ++i) { //i <= n
                 if (n % i == 0) {
                     out.push_back(i);
